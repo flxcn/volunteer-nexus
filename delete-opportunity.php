c036a2b306f286349f8ad44270127e7b38adb95a
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-08-03 17:01:51 -0500

    Update delete-opportunity.php

--- delete-opportunity.php
+++ delete-opportunity.php
@@ -1,24 +1,27 @@
 <?php
 
 // Process delete operation after confirmation
-if(isset($_POST["event_id"]) && !empty($_POST["event_id"])){
+if(isset($_POST["event_id"]) && !empty($_POST["opportunity_id"])){
   // Include config file
   require_once "config.php";
 
   // Prepare a delete statement
-  $sql = "DELETE FROM events WHERE event_id = ?";
+  $sql = "DELETE FROM opportunities WHERE opportunity_id = ?";
 
   if($stmt = mysqli_prepare($link, $sql)){
     // Bind variables to the prepared statement as parameters
-    mysqli_stmt_bind_param($stmt, "i", $param_event_id);
+    mysqli_stmt_bind_param($stmt, "i", $param_opportunity_id);
 
     // Set parameters
     $param_event_id = trim($_POST["event_id"]);
+    $param_opportunity_id = trim($_POST["opportunity_id"]);
+
 
     // Attempt to execute the prepared statement
     if(mysqli_stmt_execute($stmt)){
-      // Records deleted successfully. Redirect to landing page
-      header("location: index.php");
+      // Opportunity deleted successfully. Redirect to landing page
+      //NOTE: link may be broken
+      header('location: read.php?event_id=". $param_event_id ."');
       exit();
     } else{
       echo "Oops! Something went wrong. Please try again later.";
@@ -32,7 +35,7 @@ if(isset($_POST["event_id"]) && !empty($_POST["event_id"])){
   mysqli_close($link);
 } else{
   // Check existence of id parameter
-  if(empty(trim($_GET["event_id"]))){
+  if(empty(trim($_GET["opportunity_id"]))){
     // URL doesn't contain id parameter. Redirect to error page
     header("location: error.php");
     exit();
@@ -58,15 +61,15 @@ if(isset($_POST["event_id"]) && !empty($_POST["event_id"])){
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-header">
-                        <h1>Delete Event</h1>
+                        <h1>Delete Opportunity</h1>
                     </div>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="alert alert-danger fade in">
-                            <input type="hidden" name="event_id" value="<?php echo trim($_GET["event_id"]); ?>"/>
-                            <p>Are you sure you want to delete this record?</p><br>
+                            <input type="hidden" name="event_id" value="<?php echo trim($_GET["opportunity_id"]); ?>"/>
+                            <p>Are you sure you want to delete this opportunity?</p><br>
                             <p>
                                 <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="index.php" class="btn btn-default">No</a>
+                                <a href="read.php" class="btn btn-default">No</a>
                             </p>
                         </div>
                     </form>
