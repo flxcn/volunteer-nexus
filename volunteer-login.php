2b4e445fde2a6bb6598b5e060b3f26acdc621f85
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-08-10 17:56:19 -0500

    Update volunteer-login.php
    switch username functionality from student_id to email address

--- volunteer-login.php
+++ volunteer-login.php
@@ -4,7 +4,7 @@ session_start();
 
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
+    header("location: volunteer-welcome.php");
     exit;
 }
 
@@ -12,17 +12,19 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 require_once "config.php";
 
 // Define variables and initialize with empty values
-$student_id = $password = "";
-$student_id_error = $password_error = "";
+$username = "";
+$password = "";
+$username_error = "";
+$password_error = "";
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     // Check if username is empty
-    if(empty(trim($_POST["student_id"]))){
-        $student_id_error = "Please enter your student ID.";
+    if(empty(trim($_POST["username"]))){
+        $username_error = "Please enter your email address.";
     } else{
-        $student_id = trim($_POST["student_id"]);
+        $username = trim($_POST["username"]);
     }
 
     // Check if password is empty
@@ -35,14 +37,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_error) && empty($password_error)){
         // Prepare a select statement
-        $sql = "SELECT student_id, password FROM volunteers WHERE student_id = ?";
+        $sql = "SELECT student_id, first_name, last_name, graduation_year, username, password FROM volunteers WHERE username = ?";
 
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "i", $param_student_id);
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
 
             // Set parameters
-            $param_student_id = $student_id;
+            $param_username = $username;
 
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
@@ -52,7 +54,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $student_id, $hashed_password);
+                    mysqli_stmt_bind_result($stmt, $student_id, $first_name, $last_name, $graduation_year, $username, $hashed_password);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
@@ -61,18 +63,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["student_id"] = $student_id;
+                            $_SESSOPM["username"] = $username;
+                            $_SESSION["first_name"] = $first_name;
+                            $_SESSION["last_name"] = $last_name;
+                            $_SESSION["graduation_year"] = $graduation_year;
                             //$_SESSION["name"] = $username;
 
                             // Redirect user to welcome page
-                            header("location: welcome.php");
+                            header("location: volunteer-welcome.php");
                         } else{
                             // Display an error message if password is not valid
-                            $password_err = "The password you entered was not valid.";
+                            $password_error = "The password you entered was not valid.";
                         }
                     }
                 } else{
                     // Display an error message if username doesn't exist
-                    $student_id_error = "No account found with that student ID.";
+                    $username_error = "No account found with that email.";
                 }
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
@@ -104,10 +110,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <h2>Volunteer Login</h2>
         <p>Please fill in your credentials to login.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($student_id_error)) ? 'has-error' : ''; ?>">
-                <label>Student ID</label>
-                <input type="number" name="student_id" class="form-control" value="<?php echo $student_id; ?>">
-                <span class="help-block"><?php echo $student_id_error; ?></span>
+            <div class="form-group <?php echo (!empty($username_error)) ? 'has-error' : ''; ?>">
+                <label>Email Address</label>
+                <input type="email" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_error; ?></span>
             </div>
             <div class="form-group <?php echo (!empty($password_error)) ? 'has-error' : ''; ?>">
                 <label>Password</label>
