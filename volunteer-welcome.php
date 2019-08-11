422ac05c758492a06b97eac187669fc683ca9cf2
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-08-10 21:42:11 -0500

    Update volunteer-welcome.php

--- volunteer-welcome.php
+++ volunteer-welcome.php
@@ -4,7 +4,7 @@ session_start();
 
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
+    header("location: volunteer-login.php");
     exit;
 }
 ?>
@@ -21,7 +21,13 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 </head>
 <body>
     <div class="page-header">
-        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["first_name"]); ?></b>. Welcome to our site.</h1>
+    </div>
+    <div class="">
+        <h1>Each affiliation and the total, with glyphicons so that I can look closer at each thing.</h1>
+    </div>
+    <div class="">
+        <h1>Upcoming events that I am signed up for, with glyphicon to read, and one to cancel.</h1>
     </div>
     <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
