db8a805eaa33f18450768f48df1e9158d61d7968
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-08-12 19:23:33 -0500

    Update volunteer-welcome.php
    Fix broken link to reset password page

--- volunteer-welcome.php
+++ volunteer-welcome.php
@@ -30,7 +30,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         <h1>Upcoming events that I am signed up for, with glyphicon to read, and one to cancel.</h1>
     </div>
     <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="volunteer-reset.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
     </p>
 </body>
