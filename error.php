f83ee946b60a5f1f2c72c2521338d1baa1d61071
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-08-14 23:33:36 -0500

    Update error.php

--- error.php
+++ error.php
@@ -20,7 +20,7 @@
                         <h1>Invalid Request</h1>
                     </div>
                     <div class="alert alert-danger fade in">
-                        <p>Sorry, you've made an invalid request. Please <a href="../index.php" class="alert-link">go back</a> and try again.</p>
+                        <p>Sorry, you've made an invalid request. Please <a href="index.php" class="alert-link">go back</a> and try again.</p>
                     </div>
                 </div>
             </div>
