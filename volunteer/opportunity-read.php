b72d2770fddc986a66340d45c2866bc09753f9fe
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-08-15 22:38:49 -0500

    Update opportunity-read.php
    Move engagement creation php code to separate file

--- volunteer/opportunity-read.php
+++ volunteer/opportunity-read.php
@@ -56,38 +56,6 @@ if(isset($_GET["event_id"]) && isset($_GET["opportunity_id"])){
     // Close statement
     mysqli_stmt_close($stmt);
 
-
-
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-        // Check input errors before inserting in database
-        if(isset($_GET["opportunity_id"]) && isset($_SESSION["student_id"])){
-          // Prepare an insert statement
-            $sql = "INSERT INTO engagements (opportunity_id, student_id) VALUES (?, ?)";
-
-            if($stmt = mysqli_prepare($link, $sql)){
-                // Bind variables to the prepared statement as parameters
-                mysqli_stmt_bind_param($stmt, "ii", $param_opportunity_id, $param_student_id);
-
-                // Set parameters
-                $param_opportunity_id = $_GET["opportunity_id"];
-                $param_student_id = $_SESSION["student_id"];
-
-                // Attempt to execute the prepared statement
-                if(mysqli_stmt_execute($stmt)){
-                    // Records created successfully. Redirect to landing page
-                    header("location: events.php"); //NOTE: this can redirect to my upcoming events page
-                    exit();
-                } else{
-                    echo "Something went wrong. Please try again later. If the issue persists, send an email to westlakestuco@gmail.com detailing the problem.";
-                }
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-
-    }
     // Close connection
     mysqli_close($link);
 
@@ -152,8 +120,10 @@ if(isset($_GET["event_id"]) && isset($_GET["opportunity_id"])){
                     </div>
 
                     <div class="form-group">
-                      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <input type="submit" class="btn btn-primary" value="Submit">
+                      <form action="engagement-create.php" method="post">
+                        <input type="hidden" name="opportunity_id" value="<?php echo trim($_GET["opportunity_id"]); ?>">
+                        <input type="hidden" name="student_id" value="<?php echo trim($_SESSION["student_id"]); ?>">
+                        <input type="submit" class="btn btn-primary" value="Sign up!">
                       </form>
                     </div>
 
