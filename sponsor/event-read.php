d11d2dd3bee3f439ce5d074e4ff94f1f805633fc
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-09-02 17:34:27 -0500

    Update event-read.php

--- sponsor/event-read.php
+++ sponsor/event-read.php
@@ -170,9 +170,10 @@ if(isset($_GET["event_id"]) && !empty(trim($_GET["event_id"]))){
 
                     <?php
                     // Attempt select query execution
-                    $sql = "SELECT opportunities.opportunity_id, role_name, description, start_date, start_time, end_date, end_time, total_positions, COUNT(engagement_id) AS positions_filled
+                    $sql = "SELECT opportunities.opportunity_id AS opportunity_id, opportunities.event_id AS event_id, role_name, description, start_date, start_time, end_date, end_time, total_positions, COUNT(engagement_id) AS positions_filled
                     FROM opportunities LEFT JOIN engagements ON opportunities.opportunity_id = engagements.opportunity_id
-                    WHERE opportunities.event_id = '{$_GET["event_id"]}' GROUP BY role_name, description, start_date, start_time, end_date, end_time, total_positions, opportunities.opportunity_id";
+                    WHERE opportunities.event_id = '{$_GET["event_id"]}'
+                    GROUP BY role_name, description, start_date, start_time, end_date, end_time, total_positions, opportunities.opportunity_id";
                     if($result = mysqli_query($link, $sql)){
                         if(mysqli_num_rows($result) > 0){
                             echo "<table class='table table-bordered table-striped'>";
@@ -194,9 +195,9 @@ if(isset($_GET["event_id"]) && !empty(trim($_GET["event_id"]))){
                                         echo "<td>" . $row['end_date'] . " " . $row['end_time'] . "</td>";
                                         echo "<td>" . $row['positions_filled'] . "/" . $row['total_positions'] . "</td>";
                                         echo "<td>";
-                                            echo "<a href='opportunity-read.php?event_id=". $param_event_id ."&opportunity_id=". $row['opportunity_id'] ."' title='View Opportunity' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
-                                            echo "<a href='opportunity-update.php?event_id=". $param_event_id ."&opportunity_id=". $row['opportunity_id'] ."' title='Update Opportunity' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
-                                            echo "<a href='opportunity-delete.php?event_id=". $param_event_id ."&opportunity_id=". $row['opportunity_id'] ."' title='Delete Opportunity' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+                                            echo "<a href='opportunity-read.php?event_id=" . $_GET["event_id"] . "&opportunity_id=". $row['opportunity_id'] ."' title='View Opportunity' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
+                                            echo "<a href='opportunity-update.php?event_id=" . $_GET["event_id"] . "&opportunity_id=". $row['opportunity_id'] ."' title='Update Opportunity' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+                                            echo "<a href='opportunity-delete.php?event_id=" . $_GET["event_id"] . "&opportunity_id=". $row['opportunity_id'] ."' title='Delete Opportunity' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
                                         echo "</td>";
                                     echo "</tr>";
                                 }
