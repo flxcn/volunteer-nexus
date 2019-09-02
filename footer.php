38079676cae1c598f043f20449468ebd34cd6167
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-09-02 18:10:31 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
-<footer class="footer">
+<!-- <footer class="footer">
 	<div class="container">
 	        <span class="text-muted">&copy; 2019. Created by Felix Chen.</span>
 	</div>
 
-	<!-- <address>
+	 <address>
 		<p>
 			<a href="mailto:felix@volunteernexus.com">felix@volunteernexus.com</a>
 		</p>
-	</address> -->
-</footer>
+	</address>
+</footer> -->
