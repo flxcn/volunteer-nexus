04c90caf60fc9d581d8dbbf44e495b7b39ad74a2
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-09-02 18:11:10 -0500

    Create error pages
    pages are customized for volunteers and sponsors

---
+++ volunteer/error.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../head.php';?>
+    <meta charset="UTF-8">
+    <title>Somethings wrong...</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 750px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>Something's wrong...</h1>
+                    </div>
+                    <div class="alert alert-danger fade in">
+                        <p>Look's like something went wrong. Click <a href="dashboard.php" class="alert-link">here</a> to go back to the dashboard. If the issue persists, please send an email to felix@schoolwide.net detailing what happened. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
