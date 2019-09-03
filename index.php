849dfaed5cf4a4ea5ef84ba59d432c566e2c8782
Felix <felixchen@felixs-mbp-2.eanes-ad.eanesisd.net>
2019-09-03 11:58:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,35 +6,6 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
-  <style type="text/css">
-  body{ font: 14px sans-serif; text-align: center; }
-  </style>
-</head>
-
-<body>
-  <div class="page-header">
-    <h1><b>VolunteerNexus</b></h1>
-  </div>
-
-  <p>
-    <a href="volunteer/login.php" class="btn btn-success">Volunteer Login</a>
-    <a href="sponsor/login.php" class="btn btn-success">Sponsor Login</a>
-    <a href="volunteer/register.php" class="btn btn-success">Volunteer Register</a>
-    <a href="sponsor/register.php" class="btn btn-success">Sponsor Register</a>
-  </p>
-</body>
-</html>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <title>VolunteerNexus</title>
-  <?php include 'head.php'?>
-
   <style>
   .bd-placeholder-img {
     font-size: 1.125rem;
@@ -51,10 +22,10 @@
     }
   }
   </style>
-
-  <!-- Custom styles for this template -->
+  <!-- Custom CSS for this page -->
   <link href="css/index.css" rel="stylesheet">
 </head>
+
 <body class="text-center">
   <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
     <header class="masthead mb-auto">
