f21c8433c21b42b1a6d19fd97ea6da2677e8cca1
Felix <felixchen@felixs-mbp-2.eanes-ad.eanesisd.net>
2019-09-10 14:32:56 -0500

    Update engagements.php

--- sponsor/engagements.php
+++ sponsor/engagements.php
@@ -38,17 +38,17 @@ if(!isset($_SESSION["sponsor_loggedin"]) || $_SESSION["sponsor_loggedin"] !== tr
 
     <script>
     function showStatus(id,num) {
-      if (id == "" && num=="") {
-        //document.getElementById("statusOf"+id).innerHTML = "";
+      if (id == "" && num=="")
+      {
         return;
-      } else {
-        if (window.XMLHttpRequest) {
-          // code for IE7+, Firefox, Chrome, Opera, Safari
+      }
+      else
+      {
+        if (window.XMLHttpRequest)
+        {
           xmlhttp = new XMLHttpRequest();
-        } else {
-          // code for IE6, IE5
-          xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
         }
+
         xmlhttp.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
             document.getElementById("statusOf"+id).innerHTML = this.responseText;
@@ -88,8 +88,8 @@ if(!isset($_SESSION["sponsor_loggedin"]) || $_SESSION["sponsor_loggedin"] !== tr
 
                     if($result = mysqli_query($link, $sql)){
                         if(mysqli_num_rows($result) > 0){
-                            echo "<table class='table table-bordered table-striped'>";
-                                echo "<thead>";
+                            echo "<table class='table table-hover'>";
+                                echo "<thead class='thead-dark'>";
                                     echo "<tr>";
                                         echo "<th>Name</th>";
                                         echo "<th>Opportunity</th>";
