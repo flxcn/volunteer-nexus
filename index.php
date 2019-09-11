074b9a5f167867ec01a05db6f9cc7d17eff7f44d
Felix <felixchen@felixs-mbp-2.eanes-ad.eanesisd.net>
2019-09-11 14:30:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
   <title>VolunteerNexus</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
-  <style>
-  .bd-placeholder-img {
-    font-size: 1.125rem;
-    text-anchor: middle;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-  }
-
-  @media (min-width: 768px) {
-    .bd-placeholder-img-lg {
-      font-size: 3.5rem;
-    }
-  }
-  </style>
-  <!-- Custom CSS for this page -->
-  <link href="css/index.css" rel="stylesheet">
+  <?php include 'head.php';?>
 </head>
 
-
-<body class="text-center">
-  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-    <header class="masthead mb-auto">
-      <div class="inner">
-        <h3 class="masthead-brand">VolunteerNexus</h3>
-        <nav class="nav nav-masthead justify-content-center">
-          <a class="nav-link active" href="#">Home</a>
-          <a class="nav-link" href="volunteer/login.php">Volunteer Login</a>
-          <a class="nav-link" href="volunteer/register.php">Volunteer Register</a>
-          <a class="nav-link" href="sponsor/login.php">Sponsor Login</a>
-          <a class="nav-link" href="sponsor/register.php">Sponsor Register</a>
-          <!-- <a class="nav-link" href="">About</a> -->
-        </nav>
-      </div>
-    </header>
-
-    <main role="main" class="inner cover">
-      <h1 class="cover-heading">VolunteerNexus</h1>
-      <p class="lead">Conveniently search and sign up for volunteer opportunities at Westlake High School. Find, join, and track service opportunities, all in one place.</p>
-      <p class="lead">
-        <a href="#" class="btn btn-lg btn-secondary">Sign up!</a>
-      </p>
-    </main>
+<body>
+  <div class="page-header">
+    <h1><b>VolunteerNexus</b></h1>
   </div>
+
+  <p>
+    <h2>Volunteers</h2>
+    <div class="btn-group">
+      <a href="volunteer/login.php" class="btn btn-primary"> Login </a>
+      <a href="volunteer/register.php" class="btn btn-success">Register</a>
+
+    </div>
+  </p>
+
+  <p>
+    <h2>Sponsors</h2>
+    <div class="btn-group">
+      <a href="sponsor/login.php" class="btn btn-primary"> Login </a>
+      <a href="sponsor/register.php" class="btn btn-success">Register</a>
+    </div>
+  </p>
 </body>
 </html>
