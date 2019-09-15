5001dc837d8e95bcd482c17ffeda76407a4dd68e
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-09-14 23:50:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,20 +11,11 @@
   </div>
 
   <p>
-    <h2>Volunteers</h2>
-    <div class="btn-group">
-      <a href="volunteer/login.php" class="btn btn-primary"> Login </a>
-      <a href="volunteer/register.php" class="btn btn-success">Register</a>
 
-    </div>
-  </p>
-
-  <p>
-    <h2>Sponsors</h2>
-    <div class="btn-group">
-      <a href="sponsor/login.php" class="btn btn-primary"> Login </a>
-      <a href="sponsor/register.php" class="btn btn-success">Register</a>
-    </div>
+      <span>
+        <a href="volunteer/login.php" class="btn btn-primary">I'm a Volunteer</a>
+        <a href="sponsor/login.php" class="btn btn-primary">I'm a Sponsor</a>
+      </span>
   </p>
 </body>
 </html>
