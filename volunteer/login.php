25cf567b78c443f2cf472e78508b5953b750fe66
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-09-14 22:20:09 -0500

    Update login
    add check for session_start()

--- volunteer/login.php
+++ volunteer/login.php
@@ -58,7 +58,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
-                            session_start();
+                            if(session_status() !== PHP_SESSION_ACTIVE) session_start();
 
                             // Store data in session variables
                             $_SESSION["volunteer_loggedin"] = true;
