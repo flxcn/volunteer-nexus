b15097656563bffce61705336a423cca1674e4fa
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-09-22 23:03:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
   </div>
 
   <p>
-
       <span>
         <a href="volunteer/login.php" class="btn btn-primary">I'm a Volunteer</a>
         <a href="sponsor/login.php" class="btn btn-primary">I'm a Sponsor</a>
