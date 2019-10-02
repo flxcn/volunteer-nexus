f526a63bd9dc1dac5e859f4a16bf87671711a11d
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-10-01 22:35:07 -0500

    Update affiliations.php

--- volunteer/affiliations.php
+++ volunteer/affiliations.php
@@ -31,7 +31,7 @@
                 <div class="col-md-12">
                     <div class="page-header clearfix">
                         <h2 class="pull-left">My Affiliations</h2>
-                        <a href="affiliation-create.php" class="btn btn-success pull-right">Add New Affiliation</a>
+                        <!-- <a href="affiliation-create.php" class="btn btn-success pull-right">Add New Affiliation</a> -->
                     </div>
 
                     <?php
