3b6425bf39d6fcb114dd1944551c931cc91d2fed
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-10-26 20:27:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,13 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <!-- (START) For Local -->
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="all">
+
+<script src="jQuery/jquery-3.4.1.min.js"></script>
+
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<!-- (END) For Local -->
 
   <style type="text/css">
       .jumbotron
