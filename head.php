0870279e8e7c1e25ac39acb68f96dabbf6bb81f9
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-10-26 21:03:31 -0500

    Update head.php

--- head.php
+++ head.php
@@ -2,43 +2,15 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> -->
-
-<!-- (START) With Access to CDN -->
-
-<!-- Latest compiled and minified CSS -->
-<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous"> -->
-
-<!-- Optional theme -->
-<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous"> -->
-
-<!-- jQuery -->
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
-
-<!-- Latest compiled and minified JavaScript -->
-<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script> -->
-
-<!-- (END) With Access to CDN -->
 
 <!-- (START) For Local -->
-<link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
+<link href="../bootstrap/css/bootstrap.css" rel="stylesheet" media="all">
 
-<link href="bootstrap/css/bootstrap-theme.css" rel="stylesheet" />
+<script src="../jQuery/jquery-3.4.1.min.js"></script>
 
-<script src="jQuery/jquery-3.4.1.min.js"></script>
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+<!-- (END) For Local -->
 
-<script src="bootstrap/js/bootstrap.min.js"></script>
-<!-- (START) For Local -->
-
-<!-- Local if CDN not accessible -->
-<!-- <script>
-  if (! $.fn.modal) {
-    document.write("</script><script src='js/bootstrap.min.js'></script>");
-    document.write("<link rel='stylesheet' href='css/bootstrap.min.css'><script>");
-  }
-</script> -->
 
 <style type="text/css">
 
