d6d147fabb6f1601938ca3577f000aa0fcdb56a3
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-10-30 00:15:25 -0500

    Update events.php

--- volunteer/events.php
+++ volunteer/events.php
@@ -12,15 +12,12 @@ if(!isset($_SESSION["volunteer_loggedin"]) || $_SESSION["volunteer_loggedin"] !=
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <title>Find Events</title>
-
-
-        <!--Load required libraries-->
-        <?php include '../head.php'?>
-
+    <!--Load required libraries-->
+    <?php include '../head.php'?>
+    <!-- CSS -->
     <style type="text/css">
         .wrapper{
             margin: 0 auto;
@@ -29,83 +26,75 @@ if(!isset($_SESSION["volunteer_loggedin"]) || $_SESSION["volunteer_loggedin"] !=
             margin-top: 0;
         }
     </style>
+    <!-- Toggle Bootstrap tooltips -->
     <script type="text/javascript">
         $(document).ready(function(){
             $('[data-toggle="tooltip"]').tooltip();
         });
     </script>
 </head>
-
-
-
-
-
 <body>
-
-  <!--Navigation Bar-->
+  <!-- Navbar -->
   <?php $thisPage='Events'; include 'navbar.php';?>
 
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header clearfix">
-                        <h2 class="pull-left">All Events</h2>
-                        <!-- <a href="affiliation-create.php" class="btn btn-success pull-right">Add New Affiliation</a> -->
-                    </div>
+  <div class="wrapper">
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-md-12">
+                  <div class="page-header clearfix">
+                      <h2 class="pull-left">All Events</h2>
+                      <!-- <a href="affiliation-create.php" class="btn btn-success pull-right">Add New Affiliation</a> -->
+                  </div>
 
-                    <?php
-                    // Include config file
-                    require_once "../config.php";
-                    // Attempt select query execution
-                    $sql = "SELECT * FROM events WHERE registration_start <= CURDATE() AND registration_end >= CURDATE() ORDER BY registration_end";
-                    if($result = mysqli_query($link, $sql)){
-                        if(mysqli_num_rows($result) > 0){
-                            echo "<table class='table table-bordered table-responsive'>";
-                                echo "<thead>";
-                                    echo "<tr>";
-                                        echo "<th>Reg. Deadline</th>";
-                                        echo "<th>Event Name</th>";
-                                        echo "<th>Sponsor Name</th>";
-                                        echo "<th>Description</th>";
-                                        echo "<th>Location</th>";
-                                        echo "<th>Event Duration</th>";
-                                        echo "<th>Action</th>";
-                                    echo "</tr>";
-                                echo "</thead>";
-                                echo "<tbody>";
-                                while($row = mysqli_fetch_array($result)){
-                                    echo "<tr>";
-                                        echo "<td>" . $row['registration_end'] . "</td>";
-                                        echo "<td>" . $row['event_name'] . "</td>";
-                                        echo "<td>" . $row['sponsor_name'] . "</td>";
-                                        echo "<td>" . $row['description'] . "</td>";
-                                        echo "<td>" . $row['location'] . "</td>";
-                                        echo "<td>" . $row['event_start'] . " to " . $row['event_end'] . "</td>";
-                                        echo "<td>";
-                                            echo "<a href='event-read.php?event_id=". $row['event_id'] ."' title='View Event' data-toggle='tooltip' class='btn btn-link' ><span class='glyphicon glyphicon-eye-open'></span> View</a>";
-                                        echo "</td>";
-                                    echo "</tr>";
-                                }
-                                echo "</tbody>";
-                            echo "</table>";
-                            // Free result set
-                            mysqli_free_result($result);
-                        } else{
-                            echo "<p class='lead'><em>No events were found.</em></p>";
-                        }
-                    } else{
-                        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-                    }
+                  <?php
+                  require_once "../config.php";
 
-                    // Close connection
-                    mysqli_close($link);
-                    ?>
-                </div>
-            </div>
-        </div>
-    </div>
+                  $sql = "SELECT * FROM events WHERE registration_start <= CURDATE() AND registration_end >= CURDATE() ORDER BY registration_end";
+                  if($result = mysqli_query($link, $sql)){
+                      if(mysqli_num_rows($result) > 0){
+                          echo "<table class='table table-bordered table-responsive'>";
+                              echo "<thead>";
+                                  echo "<tr>";
+                                      echo "<th>Reg. Deadline</th>";
+                                      echo "<th>Event Name</th>";
+                                      echo "<th>Sponsor Name</th>";
+                                      echo "<th>Description</th>";
+                                      echo "<th>Location</th>";
+                                      echo "<th>Event Duration</th>";
+                                      echo "<th>Action</th>";
+                                  echo "</tr>";
+                              echo "</thead>";
+                              echo "<tbody>";
+                              while($row = mysqli_fetch_array($result)){
+                                  echo "<tr>";
+                                      echo "<td>" . $row['registration_end'] . "</td>";
+                                      echo "<td>" . $row['event_name'] . "</td>";
+                                      echo "<td>" . $row['sponsor_name'] . "</td>";
+                                      echo "<td>" . $row['description'] . "</td>";
+                                      echo "<td>" . $row['location'] . "</td>";
+                                      echo "<td>" . $row['event_start'] . " to " . $row['event_end'] . "</td>";
+                                      echo "<td>";
+                                          echo "<a href='event-read.php?event_id=". $row['event_id'] ."' title='View Event' data-toggle='tooltip' class='btn btn-link' ><span class='glyphicon glyphicon-eye-open'></span> View</a>";
+                                      echo "</td>";
+                                  echo "</tr>";
+                              }
+                              echo "</tbody>";
+                          echo "</table>";
+                          mysqli_free_result($result);
+                      } else {
+                          echo "<p class='lead'><em>No events were found.</em></p>";
+                      }
+                  } else {
+                      echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                  }
 
-    <?php include '../footer.php';?>
+                  mysqli_close($link);
+                  ?>
+              </div>
+          </div>
+      </div>
+  </div>
+
+  <?php include '../footer.php';?>
 </body>
 </html>
