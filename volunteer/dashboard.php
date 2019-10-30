85060ed2e6d3276bce2b2174ceb06f92aa1c2d62
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-10-30 00:15:59 -0500

    Update dashboard.php

--- volunteer/dashboard.php
+++ volunteer/dashboard.php
@@ -12,14 +12,11 @@ if(!isset($_SESSION["volunteer_loggedin"]) || $_SESSION["volunteer_loggedin"] !=
 <head>
     <meta charset="UTF-8">
     <title>Dashboard</title>
-
     <!--Load required libraries-->
     <?php include '../head.php'?>
 </head>
 <body>
-
     <?php $thisPage='Dashboard'; include 'navbar.php';?>
-
 
     <div class="page-header">
         <h1>Howdy, <b><?php echo htmlspecialchars($_SESSION["first_name"]); ?></b>!</h1>
@@ -32,7 +29,5 @@ if(!isset($_SESSION["volunteer_loggedin"]) || $_SESSION["volunteer_loggedin"] !=
     <?php include 'affiliations.php';?>
     <?php include 'engagements.php';?>
     <?php include '../footer.php';?>
-
-
 </body>
 </html>
