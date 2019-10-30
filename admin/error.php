d434875c20763c3d33486a341c387907e2cdeaf0
Felix <felixchen@Felixs-MBP-2.attlocal.net>
2019-10-30 00:14:25 -0500

    Create error.php

---
+++ admin/error.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../head.php';?>
+    <title>Somethings wrong...</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>Something's wrong...</h1>
+                    </div>
+                    <div class="alert alert-danger fade in">
+                        <p>Look's like something went wrong. Click <a href="dashboard.php" class="alert-link">here</a> to go back to the dashboard. If the issue persists, please send an email to <a href="mailto:felix@volunteernexus.com?Subject=VolunteerNexus%20Issue%20Report" target="_blank">felix@volunteernexus.com</a> detailing what happened. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
