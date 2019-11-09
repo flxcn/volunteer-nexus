1a14728ea6de1398a5df895426187c8b9101080b
Felix <>
2019-11-08 23:43:46 -0600

    Update engagements.php
    Fix time comparison bug; Pending Engagements page now displays the engagements whose corresponding opportunities have **already begun**.

--- sponsor/engagements.php
+++ sponsor/engagements.php
@@ -78,7 +78,7 @@ if(!isset($_SESSION["sponsor_loggedin"]) || $_SESSION["sponsor_loggedin"] !== tr
 
                     $sql = "SELECT opportunities.start_date AS start_date, engagements.engagement_id AS engagement_id, engagements.time_submitted, volunteers.first_name AS first_name, volunteers.last_name AS last_name, events.event_name AS event_name, opportunities.role_name AS role_name
                     FROM engagements LEFT JOIN volunteers ON volunteers.volunteer_id = engagements.volunteer_id LEFT JOIN events ON events.event_id = engagements.event_id LEFT JOIN opportunities ON opportunities.opportunity_id = engagements.opportunity_id
-                    WHERE engagements.sponsor_id = '{$_SESSION['sponsor_id']}' AND engagements.status IS NULL AND opportunities.start_date >= CURDATE()
+                    WHERE engagements.sponsor_id = '{$_SESSION['sponsor_id']}' AND engagements.status IS NULL AND opportunities.start_date <= CURDATE()
                     GROUP BY engagements.time_submitted, engagements.engagement_id, events.event_name, opportunities.role_name, volunteers.first_name, volunteers.last_name";
 
                     if($result = mysqli_query($link, $sql)){
