3403cebd111af27654b808f860c99211a75b827f
Felix <>
2019-11-27 21:48:59 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,36 +1,18 @@
 <?php
+$host = 'localhost';
+$db   = 'volunteer_nexus';
+$user = 'root';
+$pass = 'root';
+$charset = 'utf8mb4';
 
-define('HOST', 'localhost');
-define('USERNAME', 'root');
-define('PASSWORD', 'root');
-define('DB_NAME', 'volunteer_nexus');
-
-/*only for MAMP server (testing)*/
-define('PORT', 8889);
-
-$link = mysqli_connect(HOST, USERNAME, PASSWORD, DB_NAME, PORT);
-
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+     $this->pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
-
-/*only for testing; reports all errors, disable in release*/
-ini_set('display_errors',1); error_reporting(E_ALL);
-
-
-/*for bluehost*/
-/*
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'westlal0_vendor');
-define('DB_PASSWORD', 'IBMmodelf');
-define('DB_NAME', 'westlal0_volunteerconnect');
-
-/* Attempt to connect to MySQL database
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}*/
-
-?>
