9e1b92d5db4687c054f8b5711c426d849fff9c4c
Felix <>
2019-12-03 18:04:01 -0600

    Update attendance-anywhere-process.php
    Rewrite mysqli functions to OOP

--- sponsor/attendance-anywhere-process.php
+++ sponsor/attendance-anywhere-process.php
@@ -8,50 +8,29 @@ if(!isset($_SESSION["sponsor_loggedin"]) || $_SESSION["sponsor_loggedin"] !== tr
     exit;
 }
 
-// Include config file
-require_once '../config.php';
+require_once '../classes/AttendanceAnywhere.php';
 
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-    // Check input errors before inserting in database
-    if(isset($_POST["event_id"]) && isset($_POST["opportunity_id"]) && isset($_POST["student_id"]) && isset($_POST["sponsor_id"]) && isset($_POST["contribution_value"]) && isset($_POST["status"]))
-    {
-        // Prepare an insert statement
-        $sql = "INSERT INTO engagements (event_id, opportunity_id, volunteer_id, sponsor_id, contribution_value, status) VALUES (?, ?, ?, ?, ?, ?)";
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  if($_POST["event_id"] && $_POST["opportunity_id"] && $_POST["student_id"] && $_POST["sponsor_id"] && $_POST["contribution_value"] && $_POST["status"]) {
+    $event_id = $_POST["event_id"];
+    $opportunity_id = $_POST["opportunity_id"];
+    $volunteer_id = $_POST["volunteer_id"];
+    $sponsor_id = $_POST["sponsor_id"];
+    $contribution_value = $_POST["contribution_value"];
+    $status = $_POST["status"];
 
-        if($stmt = mysqli_prepare($link, $sql))
-        {
-              // Bind variables to the prepared statement as parameters
-              mysqli_stmt_bind_param($stmt, "iiiiii", $param_event_id, $param_opportunity_id, $param_volunteer_id, $param_sponsor_id, $param_contribution_value, $param_status);
-
-              // Set parameters
-              $param_event_id = $_POST["event_id"];
-              $param_opportunity_id = $_POST["opportunity_id"];
-              $param_volunteer_id = $_POST["volunteer_id"];
-              $param_sponsor_id = $_POST["sponsor_id"];
-              $param_contribution_value = $_POST["contribution_value"];
-              $param_status = $_POST["status"];
-
-
-              // Attempt to execute the prepared statement
-              if(mysqli_stmt_execute($stmt))
-              {
-                  // Records created successfully. Redirect to landing page
-                  echo "Success! Check-in complete!";
-                  exit();
-              }
-              else
-              {
-                echo "Something went wrong. Please try again later. If the issue persists, send an email to felix@volunteernexus.com detailing the problem.";
-              }
-
-          }
-
-          // Close statement
-          mysqli_stmt_close($stmt);
+    $obj = new AttendanceAnywhere($sponsor_id, $event_id, $opportunity_id, $contribution_value);
+    $volunteer_id_error = $obj->setVolunteerId($volunteer_id);
+    if(!$volunteer_id_error) {
+      $status = $obj->confirmAttendance();
+      if($status) {
+        echo "Success! Check-in complete!";
+      }
+      else {
+        echo "Error. Please try again.";
+      }
     }
-
-  mysqli_close($link);
+  }
 }
 
 ?>
