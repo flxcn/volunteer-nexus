98f0a14a8d985bd060e7a477046ea8d24a95f857
Felix <>
2019-12-13 22:58:54 -0600

    Create DBConnection.php

---
+++ classes/DBConnection.php
@@ -0,0 +1,32 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+define('DB_HOST','localhost');
+define('DB_NAME','volunteer_nexus');
+define('DB_CHARSET','utf8mb4');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','root');
+
+class DBConnection
+{
+    protected static $instance;
+    protected $pdo;
+
+    public function __construct() {
+			$options = [
+					PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+					PDO::ATTR_EMULATE_PREPARES   => false,
+			];
+      $dsn = 'mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset='.DB_CHARSET;
+      $this->pdo = new PDO($dsn, DB_USERNAME, DB_PASSWORD, $opt);
+    }
+
+    public function getPDO(): object
+		{
+			return $this->pdo;
+		}
+}
+?>
