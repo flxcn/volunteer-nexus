122051e4b8674c2215ee8bd6ee239929b5080bd0
Felix <>
2019-12-14 23:08:23 -0600

    Update engagement-verify.php
    OOP Rewrite

--- sponsor/engagement-verify.php
+++ sponsor/engagement-verify.php
@@ -13,48 +13,40 @@ if(!isset($_SESSION["sponsor_loggedin"]) || $_SESSION["sponsor_loggedin"] !== tr
     exit;
 }
 
-// Include config file
-require_once '../config.php';
+require '../classes/EngagementVerification.php';
+
+$sponsor_id = $_SESSION["sponsor_id"];
+$obj = new EngagementVerification($sponsor_id);
 
 if($_SERVER["REQUEST_METHOD"] == "GET")
 {
-    // Check input errors before inserting in database
-    if(isset($_GET["engagement_id"]) && isset($_GET["status"]))
-    {
-        // Prepare an insert statement
-        $sql = "UPDATE engagements SET status=? WHERE engagement_id=?";
+  // Check input errors before inserting in database
+  if(isset($_GET["engagement_id"]) && isset($_GET["status"]))
+  {
+    // Set engagement_id
+    $engagement_id = $_GET["engagement_id"];
+    $obj->setEngagementId($engagement_id);
 
-        if($stmt = mysqli_prepare($link, $sql))
-        {
-              // Bind variables to the prepared statement as parameters
-              mysqli_stmt_bind_param($stmt, "ii", $param_status, $param_engagement_id);
+    // Set engagement_status
+    $engagement_status = $_GET["status"];
+    $obj->setEngagementStatus($engagement_status);
 
-              // Set parameters
-              $param_engagement_id = $_GET["engagement_id"];
-              $param_status = $_GET["status"];
-
-              // Attempt to execute the prepared statement
-              if(mysqli_stmt_execute($stmt))
-              {
-                  // Records created successfully. Redirect to landing page
-                  if($_GET["status"] == 1)
-                    echo "<b>Confirmed!</b>";
-                  if($_GET["status"] == 0)
-                    //BUG!
-                    echo "<b>Denied!</b><a onclick='showStatus(" . $row['engagement_id'] .", NULL)' class='btn btn-outline-secondary'><>Undo</a>";
-              }
-              else
-              {
-                echo "Something went wrong. Please try again later. If the issue persists, send an email to westlakestuco@gmail.com detailing the problem.";
-              }
-
-          }
-
-          // Close statement
-          mysqli_stmt_close($stmt);
+    if($obj->updateEngagement()) {
+      if($engagement_status == -1) {
+        echo "<a onclick='showStatus(" . $engagement_id .",1)' title='Confirm This Engagement' data-toggle='tooltip' class='btn btn-link' style='color:green'><span class='glyphicon glyphicon-ok'></span></a>";
+        echo "<a onclick='showStatus(" . $engagement_id .",0)' title='Deny This Engagement' data-toggle='tooltip' class='btn btn-link' style='color:red'><span class='glyphicon glyphicon-remove'></span></a>";
+      }
+      elseif($engagement_status == 0) {
+        echo "<b>Denied!</b> <a onclick='showStatus(" . $engagement_id .", -1)' class='btn btn-outline-secondary'>Undo</a>";
+      }
+      elseif($engagement_status == 1) {
+        echo "<b>Confirmed!</b> <a onclick='showStatus(" . $engagement_id .", -1)' class='btn btn-outline-secondary'>Undo</a>";
+      }
+      else {
+        echo "Error!";
+      }
     }
-
-  mysqli_close($link);
+  }
 }
 ?>
 </body>
