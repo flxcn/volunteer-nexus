b1955b27bf6d6475b0889b5efd79bc845e907c3d
Felix <>
2019-12-14 23:07:49 -0600

    Create EngagementVerification.php
    Add class that is used by Sponsors to verify Volunteer Engagements

---
+++ classes/EngagementVerification.php
@@ -0,0 +1,70 @@
+<?php
+require 'DatabaseConnection.php';
+
+class EngagementVerification {
+	protected $pdo = null;
+	private $sponsor_id;
+	private $engagement_id;
+	private $engagement_status;
+
+	public function __construct(int $sponsor_id)
+	{
+		$this->pdo = (new DatabaseConnection)->getPDO();
+		$this->sponsor_id = $sponsor_id;
+		$this->engagement_id = "";
+		$this->engagement_status = null;
+	}
+
+	public function setEngagementId(string $engagement_id): bool
+	{
+		if(empty($engagement_id)) {
+			return false;
+		}
+		else {
+			$this->engagement_id = $engagement_id;
+			return true;
+		}
+	}
+
+	public function setEngagementStatus(int $engagement_status): bool
+	{
+		if(empty($engagement_status)) {
+			return false;
+		}
+		elseif($engagement_status == -1) {
+			$this->engagement_status = null;
+			return true;
+		}
+		elseif($engagement_status == 0){
+			$this->engagement_status = false;
+			return true;
+		}
+		elseif($engagement_status == 1){
+			$this->engagement_status = true;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public function updateEngagement(): bool {
+		$sql =
+			"UPDATE
+				engagements
+			SET
+				status = :engagement_status
+			WHERE
+				sponsor_id = :sponsor_id
+				AND engagement_id = :engagement_id";
+		$stmt = $this->pdo->prepare($sql);
+		$status = $stmt->execute(
+			[
+				'engagement_status' => $this->engagement_status,
+				'sponsor_id' => $this->sponsor_id,
+				'engagement_id' => $this->engagement_id
+			]);
+		return $status;
+	}
+}
+?>
