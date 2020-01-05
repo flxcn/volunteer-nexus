357a1ea11d96c7010b481f5f157dc697f93bb9ae
Felix <30987125+flxcn@users.noreply.github.com>
2020-01-04 23:39:27 -0600

    Update google-oauth.php
    Add "Login with Google" button, change redirect url, fix links

--- volunteer/google-oauth.php
+++ volunteer/google-oauth.php
@@ -1,10 +1,10 @@
 <?php
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
 // init configuration
 $clientID = '<YOUR_CLIENT_ID>';
 $clientSecret = '<YOUR_CLIENT_SECRET>';
-$redirectUri = 'http:/localhost/volunteer-nexus-master/volunteers/google-oauth.php';
+$redirectUri = 'http://localhost:8888/volunteer-nexus-master/volunteer/google-oauth.php';
 
 // create Client Request to access Google API
 $client = new Google_Client();
@@ -28,6 +28,7 @@ if (isset($_GET['code'])) {
 
   // now you can use this profile info to create account in your website and make user logged in.
   // Initialize User class
+  require "../classes/VolunteerGoogleAuthentication.php";
   $obj = new VolunteerGoogleAuthentication();
 
   // Getting user profile info
@@ -52,11 +53,14 @@ if (isset($_GET['code'])) {
 	}
 
 } else {
-  $output = "<a href='".$client->createAuthUrl()."'>Google Login</a>";
+  echo "<div class='col s12 m6 offset-m3 center-align'>
+    <a class='oauth-container btn darken-4 white black-text' href='".$client->createAuthUrl() ."' style='text-transform:none'>
+      <div class='left'>
+        <img width='20px' style='margin-top:7px; margin-right:8px' alt='Google sign-in'
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png' />
+      </div>
+      Login with Google
+    </a>
+  </div>";
 }
 ?>
-
-<div class="container">
-    <!-- Display login button / Google profile information -->
-    <?php echo $output; ?>
-</div>
