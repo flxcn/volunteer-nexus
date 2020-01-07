e3a24b756ff9ae87e3273d356ace591177df6de3
Felix <30987125+flxcn@users.noreply.github.com>
2020-01-06 20:53:11 -0600

    Update google-sign-in button
    Change google sign-in button link appearance

--- volunteer/google-oauth.php
+++ volunteer/google-oauth.php
@@ -53,14 +53,22 @@ if (isset($_GET['code'])) {
 	}
 
 } else {
-  echo "<div class='col s12 m6 offset-m3 center-align'>
-    <a class='oauth-container btn darken-4 white black-text' href='".$client->createAuthUrl() ."' style='text-transform:none'>
-      <div class='left'>
-        <img width='20px' style='margin-top:7px; margin-right:8px' alt='Google sign-in'
-          src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png' />
-      </div>
-      Login with Google
-    </a>
-  </div>";
+  // echo
+  //   "<!-- Compiled and minified CSS -->
+  //   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css'>
+  //   <!-- Compiled and minified JavaScript -->
+  //   <script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js'></script>";
+
+  echo
+    "<br>
+    <div class='col s12 m6 offset-m3 center-align'>
+      <a class='oauth-container btn darken-4 white black-text' href='".$client->createAuthUrl() ."' style='text-transform:none; background-color: white; border-color: black;'>
+        <div class='left'>
+          <img width='20px' style='margin-top:7px; margin-right:8px' alt='Google sign-in' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png' />
+        </div>
+        Login with Google
+      </a>
+    </div>
+    <br>";
 }
 ?>
--- volunteer/login.php
+++ volunteer/login.php
@@ -99,11 +99,5 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         </form>
     </div>
     <?php include '../footer.php';?>
-
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
 </body>
 </html>
