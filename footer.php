0ff71b98037cc9d9b1d2a65f93baca842d0d2d57
Felix <30987125+flxcn@users.noreply.github.com>
2020-01-21 21:30:22 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,20 @@
+<nav class="navbar navbar-default navbar-fixed-bottom hidden-sm hidden-xs" role="navigation">
+	<div class="container">
+		<div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li>
+					<span class="text-muted">&copy; 2020. VolunteerNexus.</span>
+				</li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li style="padding-right: 1em"><p class="navbar-text">Copyright ©&nbsp;2020 <img src="/_include/img/logo-mame-small-black.svg" alt="MAME" style="height: 1.3em; vertical-align: middle;"></p></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
 <!-- <footer class="footer">
 	<div class="container">
-	        <span class="text-muted">&copy; 2019. Created by Felix Chen.</span>
 	</div>
 
 	 <address>
