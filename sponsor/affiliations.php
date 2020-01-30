78f5dc8847fdbf0318d368ce4b9dd560ffb29d71
Felix <30987125+flxcn@users.noreply.github.com>
2020-01-29 23:13:09 -0600

    Revert "Update affiliations.php"
    This reverts commit 86b3ff59d61ab662f296b553be2df00ce92b68d8.

--- sponsor/affiliations.php
+++ sponsor/affiliations.php
@@ -5,14 +5,6 @@ if(!isset($_SESSION["sponsor_loggedin"]) || $_SESSION["sponsor_loggedin"] !== tr
     header("location: login.php");
     exit;
 }
-
-require_once "../classes/SponsorAffiliationReader.php";
-
-$sponsor_id = $_SESSION["sponsor_id"];
-$obj = new SponsorAffiliationReader($sponsor_id);
-
-$volunteers = $obj->getAffiliatedVolunteers();
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -53,33 +45,67 @@ $volunteers = $obj->getAffiliatedVolunteers();
                     </div>
 
                     <?php
-                    if($volunteers) {
-                      echo "<table class='table'>";
-                        echo "<thead>";
-                          echo "<tr>";
-                            echo "<th>Member Name</th>";
-                            echo "<th>Email Address</th>";
-                            echo "<th>Total Contributions</th>";
-                          echo "</tr>";
-                        echo "</thead>";
-                        echo "<tbody>";
-                        foreach ($volunteers as $row)
+                    // Include config file
+                    require_once "../config.php";
+
+                    // Run SQL Query
+                    $sql =
+                    "SELECT
+                      affiliations.volunteer_id AS volunteer_id,
+                      volunteers.last_name AS last_name,
+                      volunteers.first_name AS first_name,
+                      volunteers.username AS email_address,
+                      SUM(engagements.contribution_value) AS total_contribution_value
+                    FROM
+                      affiliations
+                      INNER JOIN
+                        volunteers
+                        ON affiliations.volunteer_id = volunteers.volunteer_id
+                      LEFT JOIN
+                        engagements
+                        ON affiliations.volunteer_id = engagements.volunteer_id
+                    WHERE
+                      affiliations.sponsor_id = '{$_SESSION['sponsor_id']}'
+                      AND engagements.status = '1'
+                    GROUP BY
+                      volunteers.last_name,
+                      volunteers.first_name,
+                      volunteers.username";
+
+                    if($result = mysqli_query($link, $sql)) {
+                        if(mysqli_num_rows($result) > 0) {
+                            echo "<table class='table'>";
+                                echo "<thead>";
+                                    echo "<tr>";
+                                        echo "<th>Member Name</th>";
+                                        echo "<th>Email Address</th>";
+                                        echo "<th>Total Contributions</th>";
+                                    echo "</tr>";
+                                echo "</thead>";
+                                echo "<tbody>";
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['last_name'] . ", " . $row['first_name'] . "</td>";
+                                        echo "<td>" . $row['email_address'] . "</td>";
+                                        echo "<td>" . $row['total_contribution_value'] . "</td>";
+                                        echo "<td>";
+                                            echo "<a href='affiliation-read.php?volunteer_id=". $row['volunteer_id'] ."' title='View Volunteer's Contributions' data-toggle='tooltip' class='btn btn-link'><span class='glyphicon glyphicon-eye-open'></span> View</a>";
+                                            //echo "<a href='affiliation-delete.php?affiliation_id=". $row['affiliation_id'] ."' title='Delete This Affiliation' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+                                        echo "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else
                         {
-                          echo "<tr>";
-                            echo "<td>" . $row['last_name'] . ", " . $row['first_name'] . "</td>";
-                            echo "<td>" . $row['email_address'] . "</td>";
-                            echo "<td>" . $row['total_contribution_value'] . "</td>";
-                            echo "<td>";
-                                echo "<a href='affiliation-read.php?volunteer_id=". $row['volunteer_id'] ."' title='View Volunteer's Contributions' data-toggle='tooltip' class='btn btn-link'><span class='glyphicon glyphicon-eye-open'></span> View</a>";
-                                //echo "<a href='affiliation-delete.php?affiliation_id=". $row['affiliation_id'] ."' title='Delete This Affiliation' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
-                            echo "</td>";
-                          echo "</tr>";
+                            echo "<p class='lead'><em>No affiliated volunteers were found.</em></p>";
                         }
-                        echo "</tbody>";
-                      echo "</table>";
-                    } else {
-                      echo "<p class='lead'><em>No affiliated volunteers were found.</em></p>";
+                    } else{
+                        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
                     }
+                    mysqli_close($link);
                     ?>
                 </div>
             </div>
