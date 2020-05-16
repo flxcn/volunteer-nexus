ed96d121a4b023d7d62730b13c97d1905d223725
Felix <30987125+flxcn@users.noreply.github.com>
2020-05-16 14:22:11 -0500

    Update footer.php
    Fix display to ensure footer text appears on smaller screens as well

--- footer.php
+++ footer.php
@@ -1,11 +1,37 @@
-<nav class="navbar navbar-default navbar-fixed-bottom hidden-sm hidden-xs" role="navigation">
+<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+	<div class="container">
+		<div>
+			<ul class="nav navbar-nav navbar-right">
+				<li style="padding-right: 1em">
+					<p class="navbar-text">
+						Copyright ©&nbsp;2020 Volunteer Nexus, Inc.
+					</p>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<!-- <footer class="footer">
+	<div class="container">
+	</div>
+
+	 <address>
+		<p>
+			<a href="mailto:felix@volunteernexus.com">felix@volunteernexus.com</a>
+		</p>
+	</address>
+</footer> -->
+
+
+<!-- <nav class="navbar navbar-default navbar-fixed-bottom " role="navigation">
 	<div class="container">
 		<div class="collapse navbar-collapse">
 			<ul class="nav navbar-nav navbar-left">
 				<li style="padding-left: 1em">
-					<!-- <p class="navbar-text">
+					<p class="navbar-text">
 						<span class="text-muted">&copy; 2020. Volunteer Nexus, Inc.</span>
-					</p> -->
+					</p>
 				</li>
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
@@ -17,7 +43,7 @@
 			</ul>
 		</div>
 	</div>
-</nav>
+</nav> -->
 
 <!-- <footer class="footer">
 	<div class="container">
