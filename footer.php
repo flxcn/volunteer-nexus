710c44dd9858119e406334448817c9ba64c8a116
Felix <30987125+flxcn@users.noreply.github.com>
2020-05-16 03:17:23 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,19 @@
 <nav class="navbar navbar-default navbar-fixed-bottom hidden-sm hidden-xs" role="navigation">
 	<div class="container">
 		<div class="collapse navbar-collapse">
-			<ul class="nav navbar-nav">
-				<li>
-					<span class="text-muted">&copy; 2020. VolunteerNexus.</span>
+			<ul class="nav navbar-nav navbar-left">
+				<li style="padding-left: 1em">
+					<!-- <p class="navbar-text">
+						<span class="text-muted">&copy; 2020. Volunteer Nexus, Inc.</span>
+					</p> -->
 				</li>
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
-				<li style="padding-right: 1em"><p class="navbar-text">Copyright ©&nbsp;2020 <img src="/_include/img/logo-mame-small-black.svg" alt="MAME" style="height: 1.3em; vertical-align: middle;"></p></li>
+				<li style="padding-right: 1em">
+					<p class="navbar-text">
+						Copyright ©&nbsp;2020 Volunteer Nexus, Inc.
+					</p>
+				</li>
 			</ul>
 		</div>
 	</div>
