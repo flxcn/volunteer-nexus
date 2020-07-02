42314e496c86b051164ca365423b8cdf0fae146d
Felix <30987125+flxcn@users.noreply.github.com>
2020-07-02 00:22:27 -0500

    Update EngagementCreation.php

--- classes/EngagementCreation.php
+++ classes/EngagementCreation.php
@@ -4,91 +4,116 @@ require 'DatabaseConnection.php';
 class EngagementCreation
 {
     protected $pdo = null;
-		private $volunteer_id;
-		private $sponsor_id;
-		private $event_id;
-		private $opportunity_id;
-		private $contribution_value;
-		private $status;
+	private $volunteer_id;
+	private $sponsor_id;
+	private $event_id;
+	private $opportunity_id;
+	private $contribution_value;
+	private $status;
 
-		public function __construct($sponsor_id)
+	public function __construct($sponsor_id)
     {
-      $this->pdo = (new DatabaseConnection)->getPDO();
-      $this->volunteer_id = 0;
-			$this->sponsor_id = $sponsor_id;
-			$this->event_id = 0;
-			$this->opportunity_id = 0;
-			$this->contribution_value = 0;
-			$this->status = NULL;
-		}
+    	$this->pdo = (new DatabaseConnection)->getPDO();
+    	$this->volunteer_id = 0;
+		$this->sponsor_id = $sponsor_id;
+		$this->event_id = 0;
+		$this->opportunity_id = 0;
+		$this->contribution_value = 0;
+		$this->status = NULL;
+	}
 
-		public function setVolunteerId(int $volunteer_id): string
-		{
-			if(empty($volunteer_id)) {
-				return "Please select a volunteer.";
-			}
-			else {
-				$this->$volunteer_id = $volunteer_id;
-				return "";
-			}
+	public function setVolunteerId(int $volunteer_id): string
+	{
+		if(empty($volunteer_id)) {
+			return "Please select a volunteer.";
 		}
-
-		public function setEventId(int $event_id): string
-		{
-			if(empty($event_id)) {
-				return "Please select an event.";
-			}
-			else {
-				$this->$event_id = $event_id;
-				return "";
-			}
-		}
-
-		public function setOpportunityId(int $opportunity_id): string
-		{
-			if(empty($opportunity_id)) {
-				return "Please select an opportunity.";
-			}
-			else {
-				$this->$opportunity_id = $opportunity_id;
-				return "";
-			}
-		}
-
-		public function setContributionValue(float $contribution_value): string
-		{
-			if(empty($contribution_value)) {
-				$this->$contribution_value = 0.0;
-				return "";
-			}
-			else {
-				$this->$contribution_value = $contribution_value;
-				return "";
-			}
-		}
-
-		public function setStatus(int $status): string
-		{
-			$this->$status = $status;
+		else {
+			$this->$volunteer_id = $volunteer_id;
 			return "";
 		}
+	}
+
+	public function setEventId(int $event_id): string
+	{
+		if(empty($event_id)) {
+			return "Please select an event.";
+		}
+		else {
+			$this->$event_id = $event_id;
+			return "";
+		}
+	}
+
+	public function setOpportunityId(int $opportunity_id): string
+	{
+		if(empty($opportunity_id)) {
+			return "Please select an opportunity.";
+		}
+		else {
+			$this->$opportunity_id = $opportunity_id;
+			return "";
+		}
+	}
+
+	public function setContributionValue(float $contribution_value): string
+	{
+		if(empty($contribution_value)) {
+			$this->$contribution_value = 0.0;
+			return "";
+		}
+		else {
+			$this->$contribution_value = $contribution_value;
+			return "";
+		}
+	}
+
+	public function setStatus(int $status): string
+	{
+		$this->$status = $status;
+		return "";
+	}
 
     public function addEngagement(): bool
     {
-			$sql = "INSERT INTO engagements (volunteer_id, event_id, opportunity_id, sponsor_id, contribution_value, status)
-				VALUES (:volunteer_id, :event_id, :opportunity_id, :sponsor_id, :contribution_value, :status)";
-			$stmt = $this->pdo->prepare($sql);
-			$status = $stmt->execute(
-				[
-					'volunteer_id' => $this->volunteer_id,
-					'event_id' => $this->event_id,
-					'opportunity_id' => $this->opportunity_id,
-					'sponsor_id' => $this->sponsor_id,
-					'contribution_value' => $this->contribution_value,
-					'status' => $this->status
-				]);
+		$sql = "INSERT INTO engagements (volunteer_id, event_id, opportunity_id, sponsor_id, contribution_value, status)
+			VALUES (:volunteer_id, :event_id, :opportunity_id, :sponsor_id, :contribution_value, :status)";
+		$stmt = $this->pdo->prepare($sql);
+		$status = $stmt->execute(
+			[
+				'volunteer_id' => $this->volunteer_id,
+				'event_id' => $this->event_id,
+				'opportunity_id' => $this->opportunity_id,
+				'sponsor_id' => $this->sponsor_id,
+				'contribution_value' => $this->contribution_value,
+				'status' => $this->status
+			]);
+		
+		return $status;
+	}
 
-      return $status;
-    }
+	public function getVolunteerIdByStudentId(int $student_id): string
+	{		
+		// find corresponding volunteer_ids to all student_ids
+		$sql = "SELECT volunteer_id FROM volunteers WHERE student_id = :student_id";
+		$stmt = $this->pdo->prepare($sql);
+		$status = $stmt->execute(['student_id' => $student_id]);
+		
+		return $status;
+	}
+	
+	public function addEngagementsByStudentId(array $student_ids): string
+	{
+		$output = "";
+		
+		// find corresponding volunteer_ids to all student_ids
+		// loop through INSERT sql statement to add all of the engagements with the volunteer_ids
+		// for any 
+		return $output;
+	}
+
+	public function addEngagementsByVolunteerIds(): string
+	{
+		return "";
+	}
 }
 ?>
