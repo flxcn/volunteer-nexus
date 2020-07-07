9a6faee140000061bc3f450cd3256bcb17aeb26e
Felix <30987125+flxcn@users.noreply.github.com>
2020-07-06 22:42:06 -0500

    Update affiliations.php
    Implement sortTableNumerically() function, which orders the table based on ascending or descending contribution values

--- sponsor/affiliations.php
+++ sponsor/affiliations.php
@@ -63,7 +63,7 @@ $volunteers = $obj->getAffiliatedVolunteers();
                             <th onclick='sortTable(1)' style='cursor:pointer'>
                               Email Address
                             </th>
-                            <th onclick='sortTable(2)' style='cursor:pointer'>
+                            <th onclick='sortTableNumerically(2)' style='cursor:pointer'>
                               Total Contributions
                               <a href='#'><span class='glyphicon glyphicon-sort'></span></a>
                             </th>
@@ -154,6 +154,65 @@ $volunteers = $obj->getAffiliatedVolunteers();
         }
       }
     }
+
+
+
+    function sortTableNumerically(n) {
+      var table, rows, switching, i, x, y, shouldSwitch, direction, switchcount = 0;
+      table = document.getElementById("affiliations");
+      switching = true;
+      // Set the sorting direction to ascending:
+      direction = "asc";
+      /* Make a loop that will continue until
+      no switching has been done: */
+      while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (i = 1; i < (rows.length - 1); i++) {
+          // Start by saying there should be no switching:
+          shouldSwitch = false;
+          /* Get the two elements you want to compare,
+          one from current row and one from the next: */
+          x = rows[i].getElementsByTagName("TD")[n];
+          y = rows[i + 1].getElementsByTagName("TD")[n];
+          /* Check if the two rows should switch place,
+          based on the direction, asc or desc: */
+          if (direction == "asc") {
+            if (Number(x.innerHTML.toLowerCase()) > Number(y.innerHTML.toLowerCase())) {
+              // If so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          } else if (direction == "desc") {
+            if (Number(x.innerHTML.toLowerCase()) < Number(y.innerHTML.toLowerCase())) {
+              // If so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          }
+        }
+        if (shouldSwitch) {
+          /* If a switch has been marked, make the switch
+          and mark that a switch has been done: */
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+          // Each time a switch is done, increase this count by 1:
+          switchcount ++;
+        } else {
+          /* If no switching has been done AND the direction is "asc",
+          set the direction to "desc" and run the while loop again. */
+          if (switchcount == 0 && direction == "asc") {
+            direction = "desc";
+            switching = true;
+          }
+        }
+      }
+    }
+    </script>
+
     <script>
     // search feature
     $(document).ready(function(){
