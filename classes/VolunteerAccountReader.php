a05775e743657d46e752b8e54f4995bdaf4b3eec
Felix <30987125+flxcn@users.noreply.github.com>
2020-07-07 21:44:05 -0500

    Create VolunteerAccountReader.php

---
+++ classes/VolunteerAccountReader.php
@@ -0,0 +1,102 @@
+<?php
+require_once 'DatabaseConnection.php';
+
+class VolunteerAccountReader
+{
+		protected $pdo = null;
+		private $username;
+		//private $password;
+		private $volunteer_id;
+		private $first_name;
+        private $last_name;
+        private $student_id;
+        private $graduation_year;
+        private $time_created;
+
+		public function __construct($volunteer_id)
+    {
+            $this->pdo = DatabaseConnection::instance();
+            $this->volunteer_id = "";
+            $this->username = "";
+			//$this->password = "";
+			$this->first_name = "";
+			$this->last_name = "";
+            $this->graduation_year = "";
+            $this->student_id = "";
+            $this->time_created = "";
+
+
+		}
+
+		public function getVolunteerDetails(): bool
+		{
+			$sql =
+				"SELECT
+					username,
+					first_name,
+					last_name,
+					graduation_year,
+					student_id
+				FROM
+					volunteers
+				WHERE
+					volunteer_id = :volunteer_id";
+			$stmt = $this->pdo->prepare($sql);
+			$stmt->execute(['volunteer_id' => $this->volunteer_id]);
+			$volunteer = $stmt->fetch();
+
+			if ($volunteer)
+			{
+				$this->username = $volunteer["username"];
+				$this->first_name = $volunteer["first_name"];
+				$this->last_name = $volunteer["last_name"];
+                $this->graduation_year = $volunteer["graduation_year"];
+                $this->student_id = $volunteer["student_id"];
+
+				return true;
+			} else {
+    		return false;
+			}
+		}
+
+		public function getUsername(): string
+		{
+			return $this->username;
+		}
+
+		public function getFirstName(): string
+		{
+			return $this->first_name;
+		}
+
+		public function getLastName(): string
+		{
+			return $this->last_name;
+        }
+        
+        public function getFullName(): string
+		{
+			return $this->first_name . " " . $this->last_name;
+        }
+        
+        public function getStudentId(): string
+		{
+			return $this->student_id . " " . $this->last_name;
+		}
+
+		public function getGraduationYear(): string
+		{
+			if($this->graduation_year) {
+                return $this->graduation_year;
+            } else {
+                return "N/A";
+            }
+        }
+        
+        public function getTimeCreated(): string
+		{
+			return $this->time_created;
+		}
+
+}
+?>
