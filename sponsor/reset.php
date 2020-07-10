524eec3bdb78983756e7a9d0b17673bcf6270aae
Felix <30987125+flxcn@users.noreply.github.com>
2020-07-10 00:40:49 -0500

    Update reset.php
    Change link

--- sponsor/reset.php
+++ sponsor/reset.php
@@ -104,7 +104,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
-                <a class="btn btn-link" href="dashboard.php">Cancel</a>
+                <a class="btn btn-link" href="account.php">Cancel</a>
             </div>
         </form>
     </div>
