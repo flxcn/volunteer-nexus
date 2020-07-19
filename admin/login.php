c3e4af5173df542f8d2c294a287f819a344b086f
Felix <30987125+flxcn@users.noreply.github.com>
2020-07-19 13:09:55 -0500

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,14 +1,16 @@
 <?php
+
 // Initialize the session
 session_start();
 
+// Check if the user is already logged in, if yes then redirect him to dashboard
 if(isset($_SESSION["admin_loggedin"]) && $_SESSION["admin_loggedin"] === true){
     header("location: dashboard.php");
     exit;
 }
 
 // Include config file
-require_once "../config.php";
+require_once "../classes/AdminLogin.php";
 
 // Define variables and initialize with empty values
 $username = "";
@@ -17,67 +19,38 @@ $username_error = "";
 $password_error = "";
 
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_error = "Please enter your email address.";
-    } else{
-        $username = trim($_POST["username"]);
-    }
+    // Instatiate AdminLogin object
+    $obj = new AdminLogin();
+    
+    // Set username
+    $username = trim($_POST["username"]);
+    $username_error = $obj->setUsername($username);
 
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_error = "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
+    // Set password
+    $password = trim($_POST["password"]);
+    $password_error = $obj->setPassword($password);
 
-    // Validate credentials
     if(empty($username_error) && empty($password_error)){
-        // Prepare a select statement
-        $sql = "SELECT admin_id, first_name, last_name, username, password FROM administrators WHERE username = ?";
-
-        if($query = mysqli_prepare($link, $sql)){
-            mysqli_stmt_bind_param($query, "s", $param_username);
-
-            $param_username = $username;
-
-            if(mysqli_stmt_execute($query)){
-                mysqli_stmt_store_result($query);
-
-                if(mysqli_stmt_num_rows($query) == 1){
-                    mysqli_stmt_bind_result($query, $admin_id, $first_name, $last_name, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($query)){
-                        if(password_verify($password, $hashed_password)){
-                            if(session_status() !== PHP_SESSION_ACTIVE) session_start();
-
-                            // session variables
-                            $_SESSION["admin_loggedin"] = true;
-                            $_SESSION["admin_id"] = $admin_id;
-                            $_SESSOPM["username"] = $username;
-                            $_SESSION["first_name"] = $first_name;
-                            $_SESSION["last_name"] = $last_name;
-
-                            header("location: dashboard.php");
-                        } else{
-                            $password_error = "The password you entered was not valid.";
-                        }
-                    }
-                } else{
-                    $username_error = "No account found with that email.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
+        $status = $obj->login();
+        if(!$status) {
+          $password_error = "The password you entered was not valid.";
         }
+        else {
+          // Start a new session
+          if(session_status() !== PHP_SESSION_ACTIVE) session_start();
 
-        // Close statement
-        mysqli_stmt_close($stmt);
+          // Set session variables
+          $_SESSION["admin_loggedin"] = true;
+          $_SESSION["username"] = $username;
+          $_SESSION["admin_id"] = $obj->getAdminId();
+          $_SESSION["admin_name"] = $obj->getAdminName();
+
+          // Redirect user to dashboard
+          header("location: dashboard.php");
+        }
     }
-
-    // Close connection
-    mysqli_close($link);
 }
 ?>
 
@@ -92,7 +65,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <?php include '../head.php'?>
 
     <style type="text/css">
-        body{ font: 14px sans-serif; }
         .wrapper{ width: 350px; padding: 20px; }
     </style>
 </head>
@@ -103,20 +75,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($username_error)) ? 'has-error' : ''; ?>">
                 <label>Email Address</label>
-                <input type="email" name="username" class="form-control" value="<?php echo $username; ?>" placeholder="Email">
+                <input type="email" name="username" class="form-control" placeholder="Email" value="<?php echo $username; ?>">
                 <span class="help-block"><?php echo $username_error; ?></span>
             </div>
             <div class="form-group <?php echo (!empty($password_error)) ? 'has-error' : ''; ?>">
                 <label>Password</label>
-                <input type="password" name="password" class="form-control" placeholder="Password">
+                <input type="password" name="password" placeholder="Password" class="form-control">
                 <span class="help-block"><?php echo $password_error; ?></span>
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
-            <p>Don't have an account? <a href="#">Too bad</a>.</p>
+            <p>Don't have an account? <a href="#">Too bad.</a>.</p>
         </form>
     </div>
-    <?php include '../footer.php';?>
 </body>
 </html>
