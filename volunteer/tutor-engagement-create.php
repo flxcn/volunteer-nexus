714a475925b1e0f13d1b4a8814c1d09f93e24619
Felix <30987125+flxcn@users.noreply.github.com>
2020-08-29 16:12:33 -0500

    Update tutor-engagement-create.php

--- volunteer/tutor-engagement-create.php
+++ volunteer/tutor-engagement-create.php
@@ -12,25 +12,26 @@ if(!isset($_SESSION["volunteer_loggedin"]) || $_SESSION["volunteer_loggedin"] !=
 // Define and intialize variables
 $volunteer_id = $_SESSION["volunteer_id"];
 $sponsor_id = "";
-$event_id = "";
-$opportunity_id = "";
+$date = "";
+$start_time = "";
+$end_time = "";
+$description = "";
 $contribution_value = "";
-$status = "";
 
 // Define and initialize error message variables
-$sponsor_id_error = "";
-$volunteer_name_error = "";
-$event_name_error = "";
-$opportunity_name_error = "";
+$sponsor_name_error = "";
+$date_error = "";
+$start_time_error = "";
+$end_time_error = "";
+$description_error = "";
 $contribution_value_error = '';
-$status_error = "";
 
 // Initialize EngagementFormPopulator object
 require_once '../classes/TutorEngagementFormPopulator.php';
-$tutorEngagementFormPopulatorObj = new TutorEngagementFormPopulator($sponsor_id);
+$tutorEngagementFormPopulatorObj = new TutorEngagementFormPopulator($volunteer_id);
 
 // Populate volunteer array for "volunteer name" dropdown boxes, and initialize JSON object
-$jsonSponsors = $engagementFormPopulatorObj->getSponsors();
+$jsonSponsors = $tutorEengagementFormPopulatorObj->getSponsors();
 
 
 // Process Form Submission
@@ -38,27 +39,27 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 {
 
   // Instatiate EngagementCreation object
-  require_once '../classes/EngagementCreation.php';
-  $engagementCreationObj = new EngagementCreation($sponsor_id);
+  require_once '../classes/TutorEngagementCreation.php';
+  $tutorEngagementCreationObj = new TutorEngagementCreation($sponsor_id);
 
   // Validate volunteer_id from "volunteer_name" selector 
   $volunteer_id = trim($_POST["volunteer_name"]);
-  $volunteer_name_error = $engagementCreationObj->setVolunteerId($volunteer_id);
+  $volunteer_name_error = $tutorEngagementCreationObj->setVolunteerId($volunteer_id);
 
   // Validate event_id from "event_id" selector
   $event_id = trim($_POST["event_name"]);
-  $event_name_error = $engagementCreationObj->setEventId($event_id);
+  $event_name_error = $tutorEngagementCreationObj->setEventId($event_id);
  
   // Validate opportunity_id and contribution value from "opportunity_name" selector
   $opportunity_values = json_decode($_POST["opportunity_name"]);
   $opportunity_id = $opportunity_values[0];
-  $opportunity_name_error = $engagementCreationObj->setOpportunityId($opportunity_id);
+  $opportunity_name_error = $tutorEngagementCreationObj->setOpportunityId($opportunity_id);
   $contribution_value = $opportunity_values[1];
-  $opportunity_name_error = $engagementCreationObj->setContributionValue($contribution_value);
+  $opportunity_name_error = $tutorEngagementCreationObj->setContributionValue($contribution_value);
   
   // Set status of whether the engagement needs verification
   $status = trim($_POST["status"]);
-  $engagementCreationObj->setStatus($status);
+  $tutorEngagementCreationObj->setStatus($status);
 
   // Set sponsor_id
   // $sponsor_id = $_SESSION["sponsor_id"];
@@ -66,7 +67,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
   // Check input errors before inserting in database
   if(empty($sponsor_id_error) && empty($volunteer_name_error) && empty($event_name_error) && empty($opportunity_name_error) && empty($contribution_value_error) && empty($status_error)) 
   {
-    if($engagementCreationObj->addEngagement()) {
+    if($tutorEngagementCreationObj->addEngagement()) {
       header("Location: dashboard.php");
       exit();
     }
@@ -80,11 +81,30 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Create Engagement</title>
+    <title>Create Tutor Engagement</title>
 
     <!--Load required libraries-->
     <?php $pageContent='Form'?>
     <?php include '../head.php'?>
+
+    <!-- Bootstrap Date-Picker Plugin -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+
+    <!--datepicker-->
+    <script>
+    $(document).ready(function(){
+      var date_input=$('input[type="date"]'); //our date input has the type "date"
+      var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+      var options={
+        format: 'yyyy-mm-dd',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+      };
+      date_input.datepicker(options);
+    })
+    </script>
 
     <style type="text/css">
         .wrapper{ width: 350px; padding: 20px; }
@@ -93,8 +113,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     <script type='text/javascript'>
       <?php
         echo "var sponsors = $jsonSponsors; \n";
-        echo "var events = $jsonEvents; \n";
-        echo "var opportunities = $jsonOpportunities; \n";
       ?>
 
       function loadSponsors(){
@@ -103,35 +121,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
           select.options[i] = new Option(sponsors[i].sponsor_name, sponsors[i].sponsor_id);
         }
       }
-
-      function updateOpportunities(){
-        var eventSelect = this;
-        var eventId = this.value;
-        var opportunitySelect = document.getElementById("opportunitiesSelect");
-        opportunitiesSelect.options.length = 0; // clear previous options
-        opportunitiesSelect.options[0] = new Option('Select Opportunity', "{'opportunity_id':'','contribution_value':''}");
-        if (typeof opportunities[eventId] != 'undefined') {
-          for(var i = 0; i < opportunities[eventId].length; i++){
-            var opportunityValue = [opportunities[eventId][i].opportunity_id, opportunities[eventId][i].contribution_value];
-            opportunitiesSelect.options[1+i] = new Option(opportunities[eventId][i].opportunity_name, JSON.stringify(opportunityValue));
-          }
-          opportunitySelect.onchange = updateContributionValue;
-        }
-
-      }
-
-      function updateContributionValue(){
-        var opportunitySelect = document.getElementById('opportunitiesSelect');
-        var contributionValue = document.getElementById('contributionValue');
-        var opportunityValues = JSON.parse(opportunitySelect.value);
-        contributionValue.innerHTML = opportunityValues[1];
-        // console.log(opportunityValues[1]);
-      }
     </script>
 </head>
 
 <!-- onload could be revised to be less obtrusive -->
-<body onload='loadSponsors(); loadEvents();'>
+<body onload='loadSponsors();'>
     <div class="wrapper">
         <div class="container-fluid">
             <div class="row">
@@ -143,53 +137,49 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 
                         <!--form for volunteer_name-->
-                        <div class="form-group <?php echo (!empty($volunteer_name_error)) ? 'has-error' : ''; ?>">
-                            <label>Volunteer Name</label>
-                            <p><?php echo $_SESSION["volunteer_name"];?></p>
-                            <span class="help-block"><?php echo $volunteer_name_error;?></span>
-                        </div>
-
-                        <!--form for sponsor_name-->
-                        <div class="form-group <?php echo (!empty($sponsor_name_error)) ? 'has-error' : ''; ?>">
-                            <label>Sponsor Name</label>
-                            <select name='sponsor_name' id='sponsorsSelect' class="form-control">
-                            </select>
-                            <span class="help-block"><?php echo $sponsor_name_error;?></span>
-                        </div>
-
-                        <!-- form for event_name -->
-                        <div class="form-group <?php echo (!empty($event_name_error)) ? 'has-error' : ''; ?>">
-                            <label>Event Name</label>
-                            <select name='event_name' id='eventsSelect' class="form-control">
-                            </select>
-                            <span class="help-block"><?php echo $event_name_error;?></span>
-                        </div>
-
-                        <!--form for opportunity_name-->
-                        <div class="form-group <?php echo (!empty($opportunity_name_error)) ? 'has-error' : ''; ?>">
-                            <label>Opportunity Name</label>
-                            <select name='opportunity_name' id='opportunitiesSelect' class="form-control">
-                            </select>
-                            <span class="help-block"><?php echo $opportunity_name_error;?></span>
-                        </div> -->
-
-                        <!-- display for contribution value -->
-                        <!-- NOTE: May want to allow this value to be edited for more flexibility on behalf of the sponsors -->
                         <div class="form-group">
+                            <label>Volunteer Name</label>
+                            <p><?php echo $_SESSION["first_name"] . " " . $_SESSION["last_name"];?></p>
+                        </div>
+
+                        <!--form for date-->
+                        <div class="form-group <?php echo (!empty($start_date_error)) ? 'has-error' : ''; ?>"> 
+                            <label>Date</label>
+                            <input type="date" name="start_date" class="form-control" value="<?php echo $start_date; ?>">
+                            <span class="help-block"><?php echo $start_date_error;?></span>
+                        </div>
+
+                        <!--form for start_time-->
+                        <div class="form-group <?php echo (!empty($start_time_error)) ? 'has-error' : ''; ?>">
+                            <label>Start Time</label>
+                            <input type="time" name="start_time" class="form-control" value="<?php echo $start_time; ?>">
+                            <span class="help-block"><?php echo $start_time_error;?></span>
+                        </div>
+
+                        <!--form for end_time-->
+                        <div class="form-group <?php echo (!empty($end_time_error)) ? 'has-error' : ''; ?>"> <!-- NOTE:see {2} -->
+                            <label>End Time</label>
+                            <input type="time" name="end_time" class="form-control" value="<?php echo $end_time; ?>">
+                            <span class="help-block"><?php echo $end_time_error;?></span>
+                        </div>
+
+                        <!--form for contribution_value-->
+                        <div class="form-group <?php echo (!empty($contribution_value_error)) ? 'has-error' : ''; ?>">
                             <label>Contribution Value</label>
-                            <p class="form-control-static" id='contributionValue'></p>
+                            <p>How many hours did you tutor?</p>
+                            <input type="number" min="0" step="any" name="contribution_value" class="form-control" value="<?php echo $contribution_value; ?>">
+                            <span class="help-block"><?php echo $contribution_value_error;?></span>
                         </div>
 
-                        <!--form for status-->
-                        <div class="form-group <?php echo (!empty($status_error)) ? 'has-error' : ''; ?>">
-                            <label for="status">Verified?</label>
-                            <p>Is this engagement already verified?</p>
-                            <input type="radio" name="status" value="1" checked> Yes
-                            <input type="radio" name="status" value="0"> No
-                            <span class="help-block"><?php echo $status_error;?></span>
+                        <!--form for description-->
+                        <div class="form-group <?php echo (!empty($description_error)) ? 'has-error' : ''; ?>"> <!-- NOTE:see {2} -->
+                            <label>Description</label>
+                            <p>Who did you tutor? What did you cover?</p>
+                            <textarea type="text" name="description" class="form-control"><?php echo $description; ?></textarea>
+                            <span class="help-block"><?php echo $description_error;?></span>
                         </div>
 
-
+                        <input type="hidden" name="needs_verification" value="1">
 
                         <input type="submit" class="btn btn-primary" value="Submit">
                         <a href="dashboard.php" class="btn btn-default">Cancel</a>
