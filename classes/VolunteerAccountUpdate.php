708ea2e2255f220127aeaf96b4353160436898c4
Felix <30987125+flxcn@users.noreply.github.com>
2020-09-19 15:24:46 -0500

    Update VolunteerAccountUpdate.php

--- classes/VolunteerAccountUpdate.php
+++ classes/VolunteerAccountUpdate.php
@@ -9,6 +9,7 @@ class VolunteerAccountUpdate
     private $volunteer_id;
     private $first_name;
     private $last_name;
+    private $student_id;
     private $graduation_year;
 
     public function __construct($volunteer_id)
@@ -19,6 +20,7 @@ class VolunteerAccountUpdate
         $this->password = "";
         $this->first_name = "";
         $this->last_name = "";
+        $this->student_id = "";
         $this->graduation_year = "";
     }
 
@@ -33,7 +35,64 @@ class VolunteerAccountUpdate
         }
     }
 
-	public function updateVolunteer(): bool
+    public function updateFirstName(): bool
+    {
+        $sql =
+            "UPDATE volunteers
+            SET
+                first_name = :first_name
+            WHERE
+                volunteer_id= :volunteer_id";
+        $stmt = $this->pdo->prepare($sql);
+        $status = $stmt->execute(
+            [
+                'first_name' => $this->first_name,
+                'volunteer_id' => $this->volunteer_id
+            ]
+        );
+    
+        return $status;
+    }
+
+    public function updateLastName(): bool
+    {
+        $sql =
+            "UPDATE volunteers
+            SET
+                last_name = :last_name
+            WHERE
+                volunteer_id= :volunteer_id";
+        $stmt = $this->pdo->prepare($sql);
+        $status = $stmt->execute(
+            [
+                'last_name' => $this->last_name,
+                'volunteer_id' => $this->volunteer_id
+            ]
+        );
+    
+        return $status;
+    }
+
+    public function updateStudentId(): bool
+    {
+        $sql =
+            "UPDATE volunteers
+            SET
+                student_id = :student_id
+            WHERE
+                volunteer_id= :volunteer_id";
+        $stmt = $this->pdo->prepare($sql);
+        $status = $stmt->execute(
+            [
+                'student_id' => $this->student_id,
+                'volunteer_id' => $this->volunteer_id
+            ]
+        );
+    
+        return $status;
+    }
+
+	public function updateGraduationYear(): bool
     {
         $sql =
             "UPDATE volunteers
