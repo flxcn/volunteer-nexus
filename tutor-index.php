24877b62a6039c53b443ac590245214b1cb868d7
Felix <30987125+flxcn@users.noreply.github.com>
2020-11-21 18:00:55 -0600

    Create tutor-index.php

---
+++ tutor-index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Tutoring@VolunteerNexus</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="VolunteerNexus is an online platform for finding, joining, and tracking relevant volunteer opportunities in your community.">
+
+  <!-- (START) For Local -->
+  <link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="all">
+  <script src="jQuery/jquery-3.4.1.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <!-- (END) For Local -->
+
+  <style type="text/css">
+      .jumbotron
+      {
+        text-align: center;
+      }
+
+      .container
+      {
+      }
+  </style>
+</head>
+
+<body>
+  <div class="wrapper">
+    <div class="jumbotron">
+      <img src="images/logo.png" alt="Tutoring@VolunteerNexus" style="width:80px;height:80px;"></img>
+      <h1><b>Tutoring@VolunteerNexus</b></h1>
+      <p>
+          <span>
+            <a href="https://app.volunteernexus.com/volunteer/login.php" class="btn btn-success">Tutor Login</a>
+          </span>
+      </p>
+    </div>
+
+    <div class="container">
+      <div class="page-header">
+        <h2>About</h2>
+      </div>
+      <p><i>Tutoring@VolunteerNexus</i> is a new initiative to connect and recognize the work of tutors via the VolunteerNexus platform. Now more than ever, in this time of social distancing and online learning, there exists a distinct need for students to be able to rely on peers to help engage with the material and strengthen their learning.</p>
+      <p>Interested in participating? Sign in using your VolunteerNexus account, then navigate to "Tutoring@VolunteerNexus" via your dashboard. Thanks!</p>
+
+      <div class="page-header">
+        <h2>Contact</h2>
+      </div>
+      <p>If you have any questions, please reach out to Felix at <a href="mailto:felix@volunteernexus.com">felix@volunteernexus.com</a></p>
+
+      <div class="page-header">
+        <h2>Privacy Policy</h2>
+      </div>
+      <p>Because VolunteerNexus utilizes Google's sign-in API in order to provide a more seamless user experience, we've created a privacy policy to communicate what we do with your data. You can check it out <a href="privacy-policy.php">here</a>.</p>
+    </div>
+  </div>
+</body>
+
+</html>
