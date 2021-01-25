39ac7def2bfac08ea0e99a807d7e7268c1c877b9
Felix <30987125+flxcn@users.noreply.github.com>
2021-01-25 15:07:31 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 			<ul class="nav navbar-nav navbar-right">
 				<li style="padding-right: 1em">
 					<p class="navbar-text">
-						Copyright ©&nbsp;2020 Volunteer Nexus, Inc.
+						Copyright ©&nbsp;2021 Volunteer Nexus, Inc.
 					</p>
 				</li>
 			</ul>
