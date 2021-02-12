49c4aec912efc70b2000a8330d7b1d7289d917bd
Felix <30987125+flxcn@users.noreply.github.com>
2021-02-12 17:42:25 -0600

    Update error-limit.php

--- volunteer/error-limit.php
+++ volunteer/error-limit.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <?php include '../head.php';?>
-    <title>Somethings wrong...</title>
+    <title>Sign-up Limit Reached</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         .wrapper{
@@ -19,7 +19,9 @@
                         <h1>Opportunity Sign-up Limit Reached</h1>
                     </div>
                     <div class="alert alert-danger fade in">
-                        <p>Thanks for already signing up for this opportunity! It looks like you've already signed up for this event, and you have reached the limit of times you can sign-up for this opportunity. Remember, if the contribution value isn't being added to your total, it may be due to the fact that this engagement is still pending and requires approval from the Sponsor. Click <a href="dashboard.php" class="alert-link">here</a> to go back to the dashboard. </p>
+                        <p>Thanks for already signing up for this opportunity! It looks like you've already signed up for this event, and you have reached the limit of times you can sign-up for this opportunity. Remember, if the contribution value isn't being added to your total, it may be due to the fact that this engagement is still pending and requires approval from the Sponsor.  </p>
+                        <br>
+                        <a href="dashboard.php" class="btn btn-success">Click here to go back to the dashboard.</a>
                     </div>
                 </div>
             </div>
