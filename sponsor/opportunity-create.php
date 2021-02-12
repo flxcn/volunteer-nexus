bab3782d2df6b2da42796630701df9d7d9af9ab5
Felix <30987125+flxcn@users.noreply.github.com>
2021-02-12 17:32:07 -0600

    Update opportunity-create.php
    Rewrite form processing with OOP approach (SponsorOpportunityCreation.php)

--- sponsor/opportunity-create.php
+++ sponsor/opportunity-create.php
@@ -9,7 +9,9 @@ if(!isset($_SESSION["sponsor_loggedin"]) || $_SESSION["sponsor_loggedin"] !== tr
 }
 
 // Include config file
-require_once '../config.php';
+require_once '../classes/SponsorOpportunityCreation.php';
+$sponsor_id = trim($_SESSION["sponsor_id"]);
+$obj = new SponsorOpportunityCreation($sponsor_id);
 
 // Define variable
 if(isset($_GET["event_id"])) {
@@ -18,7 +20,6 @@ if(isset($_GET["event_id"])) {
   $event_id = "";
 }
 
-$sponsor_id = trim($_SESSION["sponsor_id"]);
 $opportunity_name = "";
 $description = "";
 $start_date = "";
@@ -50,154 +51,77 @@ $needs_reminder_error = "";
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     $event_id = trim($_POST["event_id"]);
-    $sponsor_id = trim($_POST["sponsor_id"]);
+    $obj->setEventId($event_id);
 
     // Validate opportunity_name
-    $input_opportunity_name = trim($_POST["opportunity_name"]);
-    if(empty($input_opportunity_name)){
-        $opportunity_name_error = "Please enter a role name.";
-    } else{
-        $opportunity_name = $input_opportunity_name;
-    }
+    $opportunity_name = trim($_POST["opportunity_name"]);
+    $opportunity_name_error = $obj->setOpportunityName($opportunity_name);
 
     // Validate description
-    $input_description = trim($_POST["description"]);
-    if(empty($input_description)){
-        $description_error = "Please enter a description.";
-    } else{
-        $description = $input_description;
-    }
+    $description = trim($_POST["description"]);
+    $description_error = $obj->setDescription($description);
 
     // Validate start_date
-    $input_start_date = trim($_POST["start_date"]);
-    if(empty($input_start_date)){
-        $start_date_error = "Please enter a start date.";
-    } else{
-        $start_date = $input_start_date;
-    }
+    $start_date = trim($_POST["start_date"]);
+    $start_date_error = $obj->setStartDate($start_date);
 
     // Validate end_date
-    $input_end_date = trim($_POST["end_date"]);
-    if(empty($input_end_date)){
-        $end_date_error = "Please enter an end date.";
-    } else{
-        $end_date = $input_end_date;
-    }
+    $end_date = trim($_POST["end_date"]);
+    $end_date_error = $obj->setEndDate($end_date);
 
     // Validate start_time
-    $input_start_time = trim($_POST["start_time"]);
-    if(empty($input_start_time)){
-        $start_time_error = "Please enter a start time.";
-    } else{
-        // $date = DateTime::createFromFormat( 'H:i A', $input_start_time);
-        // $temp_start_time = $date->format( 'H:i:s');
-	      // $start_time = $temp_start_time;
-        $start_time = date("H:i", strtotime($input_start_time));
-    }
+    $start_time = trim($_POST["start_time"]);
+    $start_time_error = $obj->setStartTime($start_time);
 
     // Validate end_time
-    $input_end_time = trim($_POST["end_time"]);
-    if(empty($input_end_time)){
-        $end_time_error = "Please enter an end time.";
-    } else{
-      // $date = DateTime::createFromFormat( 'H:i A', $input_end_time);
-      // $temp_end_time = $date->format( 'H:i:s');
-      // $end_time = $temp_end_time;
-      $end_time = date("H:i", strtotime($input_end_time));
-    }
+    $end_time = trim($_POST["end_time"]);
+    $end_time_error = $obj->setEndTime($end_time);
 
     // Validate total_positions
-    $input_total_positions = trim($_POST["total_positions"]);
-    if(empty($input_total_positions)){
-        $total_positions_error = "Please enter the total number of positions available.";
-    } else{
-        $total_positions = $input_total_positions;
-    }
+    $total_positions = trim($_POST["total_positions"]);
+    $total_positions_error = $obj->setTotalPositions($total_positions);
 
     // Validate limit_per_volunteer
-    $input_limit_per_volunteer = trim($_POST["limit_per_volunteer"]);
-    if(empty($input_limit_per_volunteer)){
-        $limit_per_volunteer_error = "Please enter the total number of sign-ups each volunteer is allowed.";
-    } else{
-        $limit_per_volunteer = $input_limit_per_volunteer;
-    }
+    $limit_per_volunteer = trim($_POST["limit_per_volunteer"]);
+    $limit_per_volunteer_error = $obj->setLimitPerVolunteer($limit_per_volunteer);
 
     // Validate contribution_value
-    $input_contribution_value = trim($_POST["contribution_value"]);
-    if(empty($input_contribution_value)){
-        $contribution_value_error = "Please enter a contribution value.";
-    } else{
-        $contribution_value = $input_contribution_value;
-    }
+    $contribution_value = trim($_POST["contribution_value"]);
+    $contribution_value_error = $obj->setContributionValue($contribution_value);
 
     // Validate needs_verification
-    $input_needs_verification = trim($_POST["needs_verification"]);
-    $needs_verification = $input_needs_verification;
+    $needs_verification = trim($_POST["needs_verification"]);
+    $obj->setNeedsVerification($needs_verification);
 
     // Validate needs_reminder
-    $input_needs_reminder = trim($_POST["needs_reminder"]);
-    $needs_reminder = $input_needs_reminder;
-
+    $needs_reminder = trim($_POST["needs_reminder"]);
+    $obj->setNeedsReminder($needs_reminder);
 
     // Check input errors before inserting in database
-    if(empty($event_id_error) && empty($sponsor_id_error) && empty($opportunity_name_error) && empty($description_error) && empty($start_date_error) && empty($end_date_error) && empty($start_time_error) && empty($end_time_error) && empty($total_positions_error) && empty($contribution_value_error) && empty($needs_verification_error) && empty($needs_reminder_error)){
-        // Prepare an insert statement
-        $sql = "INSERT INTO opportunities (event_id, sponsor_id, opportunity_name, description, start_date, end_date, start_time, end_time, total_positions, limit_per_volunteer, contribution_value, needs_verification, needs_reminder) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "iissssssiidii", 
-                $param_event_id, 
-                $param_sponsor_id, 
-                $param_opportunity_name, 
-                $param_description, 
-                $param_start_date, 
-                $param_end_date, 
-                $param_start_time, 
-                $param_end_time, 
-                $param_total_positions,
-                $param_limit_per_volunteer, 
-                $param_contribution_value, 
-                $param_needs_verification, 
-                $param_needs_reminder
-            );
-
-            // Set parameters
-            $param_event_id = $event_id;
-            $param_sponsor_id = $sponsor_id;
-            $param_opportunity_name = $opportunity_name;
-            $param_description = $description;
-            $param_start_date = $start_date;
-            $param_end_date = $end_date;
-            $param_start_time = $start_time;
-            $param_end_time = $end_time;
-            $param_total_positions = $total_positions;
-            $param_limit_per_volunteer = $limit_per_volunteer;
-            $param_contribution_value = $contribution_value;
-            $param_needs_verification = $needs_verification;
-            $param_needs_reminder = $needs_reminder;
-
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Records created successfully. Redirect to landing page
-
-                // Close statement
-                mysqli_stmt_close($stmt);
-
-                header("Location: event-read.php?event_id=" . $event_id);
-                exit();
-            } else{
-                echo "Something went wrong. Please try again later. If the issue persists, send an email to westlakestuco@gmail.com detailing the problem.";
-            }
-
-
+    if( empty($event_id_error) 
+        && empty($sponsor_id_error) 
+        && empty($opportunity_name_error) 
+        && empty($description_error) 
+        && empty($start_date_error) 
+        && empty($end_date_error) 
+        && empty($start_time_error) 
+        && empty($end_time_error) 
+        && empty($total_positions_error) 
+        && empty($limit_per_volunteer_error) 
+        && empty($contribution_value_error) 
+        && empty($needs_verification_error) 
+        && empty($needs_reminder_error)
+    ) 
+    {
+        if($obj->addOpportunity()) 
+        {
+            header("Location: event-read.php?event_id=" . $event_id);
+            exit();
+        } else {
+            echo "Something went wrong. Please try again later. If the issue persists, send an email to felix@volunteernexus.com detailing the problem.";
         }
-
-
     }
 
-    // Close connection
-    mysqli_close($link);
 }
 ?>
 
@@ -340,6 +264,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
         </div>
     </div>
+
     <?php include '../footer.php';?>
 </body>
 </html>
