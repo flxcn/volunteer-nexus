387e762a3ca2c7cbd52ec8988b14a97c09a6011b
Felix <30987125+flxcn@users.noreply.github.com>
2021-02-12 22:13:34 -0600

    Update tutoring-guidelines.php

---
+++ volunteer/tutoring-guidelines.php
@@ -0,0 +1,56 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["volunteer_loggedin"]) || $_SESSION["volunteer_loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+// Check existence of id parameter before processing further
+if(isset($_SESSION["volunteer_id"])) {
+    $volunteer_id = $_SESSION["volunteer_id"];
+} else {
+    header("location: error.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tutoring Guidelines</title>
+
+    <!--Load required libraries-->
+    <?php include '../head.php'?>
+
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <?php $thisPage='Tutoring'; include 'navbar.php';?>
+
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <div class="page-header">
+                        <h1>Tutoring Guidelines</h1>
+                    </div>
+
+                    <p>Be sure to follow the proper guidelines.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+	
+    <?php include '../footer.php';?>
+</body>
+</html>
