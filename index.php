6154d9fc4814491fd3ec04319fb6bb431398fa35
Felix <30987125+flxcn@users.noreply.github.com>
2021-05-14 23:13:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,62 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>VolunteerNexus</title>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="VolunteerNexus is an online platform for finding, joining, and tracking relevant volunteer opportunities in your community.">
+    <title>VolunteerNexus</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="VolunteerNexus is an online platform for finding, joining, and tracking relevant volunteer opportunities in your community.">
 
-  <!-- (START) For Local -->
-  <link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="all">
-  <script src="jQuery/jquery-3.4.1.min.js"></script>
-  <script src="bootstrap/js/bootstrap.min.js"></script>
-  <!-- (END) For Local -->
+    <!-- (START) For Local -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="all">
+    <script src="jQuery/jquery-3.4.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- (END) For Local -->
 
-  <style type="text/css">
-      .jumbotron
-      {
-        text-align: center;
-      }
-
-      .container
-      {
-      }
-  </style>
+    <style type="text/css">
+        .jumbotron
+        {
+            text-align: center;
+        }
+    </style>
 </head>
 
 <body>
-  <div class="wrapper">
-    <div class="jumbotron">
-      <img src="images/logo.png" alt="VolunteerNexus" style="width:80px;height:80px;"></img>
-      <h1><b>VolunteerNexus</b></h1>
-      <p>
-          <span>
-            <a href="volunteer/login.php" class="btn btn-success">I'm a Volunteer</a>
-            <a href="sponsor/login.php" class="btn btn-success">I'm a Sponsor</a>
-          </span>
-      </p>
+    <div class="wrapper">
+        <div class="jumbotron">
+        <img src="images/logo.png" alt="VolunteerNexus" style="width:80px;height:80px;"></img>
+        <h1><b>VolunteerNexus</b></h1>
+        <p>
+            <span>
+                <a href="volunteer/login.php" class="btn btn-success">I'm a Volunteer</a>
+                <a href="sponsor/login.php" class="btn btn-success">I'm a Sponsor</a>
+            </span>
+        </p>
+        </div>
+
+        <div class="container">
+
+        <div class="panel panel-primary">
+            <div class="panel-heading">VolunteerNexus 2.0 Launch!</div>
+            <div class="panel-body">
+                We are excited to announce the next major update of the VolunteerNexus platform. 
+            </div>
+            <div class="panel-footer">May 12th, 2021</div>
+        </div>
+        
+        <div class="page-header">
+            <h2>About</h2>
+        </div>
+        <p><i>VolunteerNexus</i> provides a unified platform to connect students with service opportunities.</p>
+        <p>Currently, <i>VolunteerNexus</i> is available to use for <b>all Westlake students</b>.</p>
+
+        <div class="page-header">
+            <h2>Contact</h2>
+        </div>
+        <p>If you have any questions, please reach out to Felix at <a href="mailto:felix@volunteernexus.com">felix@volunteernexus.com</a></p>
+
+        <div class="page-header">
+            <h2>Privacy Policy</h2>
+        </div>
+            <p>Because VolunteerNexus utilizes Google's sign-in API in order to provide a more seamless user experience, we've created a privacy policy to communicate what we do with your data. You can check it out <a href="privacy-policy.php">here</a>.</p>
+        </div>
     </div>
-
-    <div class="container">
-      <div class="page-header">
-        <h2>About</h2>
-      </div>
-      <p><i>VolunteerNexus</i> provides a unified platform to connect students with service opportunities.</p>
-      <p>Currently, <i>VolunteerNexus</i> is available to use for <b>Student Council, Latin Club, Model UN, NHS</b>.</p>
-      <p>If you are interested in joining the platform in the future, fill out <a href="https://forms.gle/tEZxh4wjhZ4vvhBH9"><strong>this form</strong></a>, and we will send you an invite link when it is open. Thanks!</p>
-
-      <div class="page-header">
-        <h2>Contact</h2>
-      </div>
-      <p>If you have any questions, please reach out to Felix at <a href="mailto:felix@volunteernexus.com">felix@volunteernexus.com</a></p>
-
-      <div class="page-header">
-        <h2>Privacy Policy</h2>
-      </div>
-      <p>Because VolunteerNexus utilizes Google's sign-in API in order to provide a more seamless user experience, we've created a privacy policy to communicate what we do with your data. You can check it out <a href="privacy-policy.php">here</a>.</p>
-    </div>
-  </div>
 </body>
 
 </html>
