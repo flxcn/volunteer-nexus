5aef4c2a9b60e72c780bebe418b1c0555bc3f96e
Felix <30987125+flxcn@users.noreply.github.com>
2021-07-22 01:02:41 -0500

    Update events.php

--- volunteer/events.php
+++ volunteer/events.php
@@ -1,11 +1,9 @@
 <?php
-// Initialize the session
 session_start();
 
-// Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["volunteer_loggedin"]) || $_SESSION["volunteer_loggedin"] !== true)
 {
-    header("location: login.php");
+    header("location: sign-in.php");
     exit;
 }
 
@@ -18,41 +16,37 @@ $events = $obj->getEvents();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Find Events</title>
-    <!--Load required libraries-->
-    <?php include '../head.php'?>
-    <!-- CSS -->
-    <style type="text/css">
-        .wrapper{
-            margin: 0 auto;
-        }
-        .page-header h2{
-            margin-top: 0;
-        }
-    </style>
-    <!-- Toggle Bootstrap tooltips -->
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $('[data-toggle="tooltip"]').tooltip();
-        });
-    </script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Felix Chen">
+    
+    <title>Events</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../assets/css/events.css" rel="stylesheet">
 </head>
 <body>
-  <!-- Navbar -->
-  <?php $thisPage='Events'; include 'navbar.php';?>
+    <!-- Navbar -->
+    <?php $thisPage='Events'; include 'navbar.php';?>
 
-  <div class="wrapper">
-      <div class="container-fluid">
-          <div class="row">
-              <div class="col-md-12">
-                    <div class="page-header clearfix">
-                        <h2 class="pull-left">All Events</h2>
-                        <a href="affiliation-create.php" class="btn btn-primary pull-right">Add New Affiliation</a>
+    <div class="container-fluid">
+        <div class="row">
+            <main class="ms-sm-auto px-md-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2">All Events</h1>
+                    <div class="btn-toolbar mb-2 mb-md-0">
+                        <div class="btn-group me-2">
+                            <button type="button" class="btn btn-sm btn-outline-secondary" onclick="window.print()"><span data-feather="printer"></span> Print</button>
+                        </div>
                     </div>
+                </div>
 
-                    <?php if($events): ?>
-                    <div>
+                <?php if($events): ?>
+                    <div class="table-responsive">
                         <table class='table table-bordered table-responsive'>
                             <thead>
                                 <tr>
@@ -84,15 +78,26 @@ $events = $obj->getEvents();
                     </div>
                     <?php else: ?>
                     <p class='lead'>
-                        <em>No events were found. If you have not yet, click <a href='affiliation-create.php'>here</a> to join a Sponsor in order to view their Events.</em>
+                        <em>There are currently no events. If you have not yet, click <a href='affiliation-create.php'>here</a> to join a Sponsor in order to view their Events.</em>
                     </p>
                     <?php endif; ?>
-                </div>
-            </div>
+            </main>
+            
+            <!-- Footer -->
+            <?php include "footer.php"; ?>
         </div>
     </div>
 
-    <!-- Footer -->
-    <?php include '../footer.php';?>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+    <script src="../assets/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
+
+    <script>
+    (function () {
+    'use strict'
+
+    feather.replace({ 'aria-hidden': 'true' })
+
+    })()
+    </script>
 </body>
 </html>
