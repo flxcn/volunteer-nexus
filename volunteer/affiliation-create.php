3931f12aad5409fe54639eb174d96776192b19db
Felix <30987125+flxcn@users.noreply.github.com>
2021-07-21 23:52:10 -0500

    Update affiliation-create.php

--- volunteer/affiliation-create.php
+++ volunteer/affiliation-create.php
@@ -103,9 +103,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
         </div>
 
-        <footer class="my-5 pt-5 text-muted text-center text-small">
-            <p class="mb-1">&copy; 2021 Felix Chen</p>
-        </footer>
+        <?php include "footer.php"; ?>
     </div>
 
     <!-- Custom js for this page -->
