472b2a9b0dc013cb3077ad208891ee9f8d39f687
Felix <30987125+flxcn@users.noreply.github.com>
2021-07-21 22:37:37 -0500

    Create profile.php

---
+++ volunteer/profile.php
@@ -0,0 +1,111 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["volunteer_loggedin"]) || $_SESSION["volunteer_loggedin"] !== true){
+    header("location: sign-in.php");
+    exit;
+}
+
+// Check existence of id parameter before processing further
+if(isset($_SESSION["volunteer_id"])) {
+    
+    $volunteer_id = $_SESSION["volunteer_id"];
+    require_once "../classes/VolunteerAccountReader.php";
+    $obj = new VolunteerAccountReader($volunteer_id);
+
+    $obj->getVolunteerDetails();
+
+} else {
+    header("location: error.php");
+    exit();
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Felix Chen">
+    
+    <title>Profile</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Custom styles for this template -->
+    <link href="../assets/css/dashboard.css" rel="stylesheet">
+</head>
+<body>
+    <?php $thisPage='Profile'; include 'navbar-new.php';?>
+
+    <div class="container-fluid">
+        <div class="row">
+            
+            <main class="ms-sm-auto px-md-4">
+                
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2">Profile</h1>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-sm-6 col-lg-4 mb-4">
+                        <div class="card text-center">
+
+                            <div class="card-body">
+                            <h5 class="card-title"><?php echo $obj->getFullName(); ?></h5>
+                            <p class="card-text"><?php echo $obj->getUsername(); ?></p>
+                            <p class="card-text"><small class="text-muted">Member since <?php echo $obj->getTimeCreated(); ?></small></p>
+                            <hr>
+                            <p class="card-text"><?php echo "Class of " . $obj->getGraduationYear(); ?></p>
+                            <p class="card-text">Student ID: <?php echo $obj->getStudentId(); ?></p>
+
+                            <div class="btn-group text-center mb-2 mb-md-0">
+                                <a href="#" class="btn btn-sm btn-outline-primary">Update profile</a>  
+                                <a href="reset.php" class="btn btn-sm btn-outline-primary">Reset password</a>  
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div class="col-sm-6 col-lg-8 mb-4">
+                        <div class="card p-3">
+                            <figure class="p-3 mb-0">
+                            <blockquote class="blockquote">
+                                <p>The broadest, and maybe the most meaningful definition of volunteering: doing more than you have to because you want to, in a cause you consider good.</p>
+                            </blockquote>
+                            <figcaption class="blockquote-footer mb-0 mt-1 text-muted">
+                                Ivan Scheier
+                            </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <div class="card text-center my-2 bg-dark text-white">
+                            <div class="card-body">
+                                <h5 class="card-title">Volunteer ID</h5>
+                                <p class="card-text"><small><i>AttendanceAnywhere</i></small></p>
+                            </div>
+                            <?php include "attendance-anywhere-new.php"; ?>
+                        </div>
+                    </div>
+                </div>
+
+            </main>
+
+            <?php include "../footer.php" ?>
+        </div>
+    </div>
+
+    <script src="../assets/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
