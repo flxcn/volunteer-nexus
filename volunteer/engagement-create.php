e4919eca4aec138245c15b86fdd8b007ae87138d
Felix <30987125+flxcn@users.noreply.github.com>
2021-07-23 21:57:54 -0500

    Update engagement-create.php

--- volunteer/engagement-create.php
+++ volunteer/engagement-create.php
@@ -35,8 +35,31 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         else {
             if($obj->addEngagement()) {
                 // Success
+
+                $_SESSION["engagement_creation_success"] = true;
+
                 header("location: dashboard.php");
+
+                // $url = 'dashboard.php';
+                // $data = array('creation' => 'success');
+
+                // // use key 'http' even if you send the request to https://...
+                // $options = array(
+                //     'http' => array(
+                //         'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                //         'method'  => 'POST',
+                //         'content' => http_build_query($data)
+                //     )
+                // );
+                // $context  = stream_context_create($options);
+                // $result = file_get_contents($url, false, $context);
+                // if ($result === FALSE) { /* Handle error */ }
+
+                // var_dump($result);
+
                 exit();
+
+
             }
             else {
                 // Failure
