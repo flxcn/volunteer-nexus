e59e0e966e4a1fdd0839046c87d2ea82d7c767b8
Felix <30987125+flxcn@users.noreply.github.com>
2021-07-23 21:59:13 -0500

    Update footer.php

--- volunteer/footer.php
+++ volunteer/footer.php
@@ -1,3 +1,3 @@
-<footer class="my-5 pt-5 text-muted text-center text-small">
-    <p class="mb-1">&copy; 2021 Felix Chen</p>
+<footer class="my-5 pt-5 text-muted text-center text-small print">
+    <p class="mb-1">&copy; 2019-<?php echo date('Y'); ?> Volunteer Nexus, Inc.</p>
 </footer>
