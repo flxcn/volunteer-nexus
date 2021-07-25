5322abfc43c8e2b201d34679864b93ad539a59a4
Felix <30987125+flxcn@users.noreply.github.com>
2021-07-24 22:46:27 -0500

    Update VolunteerEngagementReader.php

--- classes/VolunteerEngagementReader.php
+++ classes/VolunteerEngagementReader.php
@@ -11,20 +11,24 @@ class VolunteerEngagementReader {
 		$this->volunteer_id = $volunteer_id;
     }
 
+    
+
 	public function getUpcomingEngagements(): ?array
 	{
 		$sql =
             "SELECT     engagements.engagement_id       AS engagement_id,
+                        engagements.contribution_value  AS contribution_value,
                         opportunities.opportunity_id    AS opportunity_id,
                         opportunities.description       AS description,
                         opportunities.start_date        AS start_date,
                         opportunities.end_date          AS end_date,
                         opportunities.start_time        AS start_time,
                         opportunities.end_time          AS end_time,
+                        opportunities.opportunity_name  AS opportunity_name,
                         events.event_name               AS event_name,
                         events.contact_name             AS contact_name,
                         events.contact_email            AS contact_email,
-                        opportunities.opportunity_name  AS opportunity_name
+                        events.contribution_type        AS contribution_type
             FROM        engagements
                         LEFT JOIN   volunteers
                                ON   volunteers.volunteer_id = engagements.volunteer_id
@@ -41,7 +45,9 @@ class VolunteerEngagementReader {
                         engagements.engagement_id,
                         events.event_name,
                         opportunities.opportunity_name,
-                        opportunities.opportunity_id";
+                        opportunities.opportunity_id,
+                        events.contribution_type,
+                        engagements.contribution_value";
 
 		$stmt = $this->pdo->prepare($sql);
 		$stmt->execute(['volunteer_id' => $this->volunteer_id]);
@@ -52,6 +58,25 @@ class VolunteerEngagementReader {
 		}
 		else {
 			return $engagements;
+		}
+	}
+
+    public function formatDate($date_string): ?string
+	{
+		$date = strtotime($date_string);
+        // return date('D, M. jS', $date);
+        return date('M. jS', $date);
+	}
+
+	public function formatOpportunityStartToEnd($event_start,$event_end): ?string
+	{
+		$date1 = $this->formatDate($event_start);
+		if (strcmp($event_start,$event_end) == 0) {
+			return $date1;
+		}
+		else {
+			$date2 = $this->formatDate($event_end);
+			return $date1 . " to " . $date2;
 		}
 	}
 }
