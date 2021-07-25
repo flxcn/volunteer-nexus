9b40e67d8b1bca6272107440e7a0f3e436ad8d13
Felix <30987125+flxcn@users.noreply.github.com>
2021-07-25 01:37:46 -0500

    Create guidelines.php

---
+++ tutor/guidelines.php
@@ -0,0 +1,31 @@
+<div class="page-header">   
+    <h1>Tutoring</h1>
+</div>
+
+<p>
+<a href="tutorial-create.php" class="btn btn-success">Record your contributions!</a>
+</p>
+
+<h2>How it works:</h2>
+
+<h3>1. Register.</h3>
+<p>
+If you want to become a tutor through a Sponsor, fill out one of the corresponding forms below linked <a href="#tutor-links">here</a> (or scroll down). Then, the Sponsor will pair you up with a student seeking tutoring help.
+</p>
+
+<p>
+If you want to be tutored through a specific Sponsor, fill out one of the corresponding forms below linked <a href="#tutor-links">here</a> (or scroll down). Then, the Sponsor will pair you up with a student who can help.
+</p>
+
+<h3>2. Tutor</h3>
+<p>
+Connect with students interested in receiving tutoring help, and work closely to help them understand the material. A list of tutoring guidelines can be found <a href="tutoring-guidelines.php">here</a>.
+</p>
+
+<h3>3. Record</h3>
+<p>
+Take a picture of yourself during tutoring as proof of participation. Send it to your respective Sponsor through their designated process.
+</p>
+<p>
+Use the button above labeled <b><a href="tutor-engagement-create.php">"Record your contributions!"</a></b> and fill out the linked form. Your affiliated Sponsor will confirm your participation, and your volunteering will be recognized! Thank you for taking the time to support the learning of others! Once approved, your tutoring work will show up in your contribution history, linked <a href="dashboard.php#affiliations">here</a>.
+</p>
