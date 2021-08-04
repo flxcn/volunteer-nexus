f1d099779e9f431e6be763b0cb1ac9ec038bb9b3
Felix <30987125+flxcn@users.noreply.github.com>
2021-08-04 16:17:22 -0500

    Update affiliation-read.php

--- sponsor/affiliation-read.php
+++ sponsor/affiliation-read.php
@@ -2,7 +2,7 @@
 session_start();
 
 if(!isset($_SESSION["sponsor_loggedin"]) || $_SESSION["sponsor_loggedin"] !== true){
-    header("location: login.php");
+    header("location: sign-in.php");
     exit;
 }
 
@@ -12,48 +12,51 @@ $obj = new SponsorAffiliationReader($_SESSION["sponsor_id"]);
 $engagements = $obj->getEngagementsForAffiliatedVolunteer($_GET['volunteer_id']);
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Felix Chen">
+    
     <title>This Volunteer's Contributions</title>
-    <!--Load required libraries-->
-    <?php include '../head.php'?>
-    <style type="text/css">
-        .wrapper{
-            margin: 0 auto;
-        }
-        .page-header h2{
-            margin-top: 0;
-        }
-    </style>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $('[data-toggle="tooltip"]').tooltip();
-        });
-    </script>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Custom styles for this template -->
+    <link href="../assets/css/main.css" rel="stylesheet">
+    <link rel="stylesheet" media="print" href="../assets/css/print.css" />
 </head>
 
 <body>
     <?php $thisPage='Affiliations'; include 'navbar.php';?>
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header clearfix">
-                        <h2 class="pull-left">Volunteer's Past Engagements for This Sponsor</h2>
-                        <a href='affiliations.php' class="btn btn-primary pull-right">Back</a>
-                    </div>
 
-                    <?php if ($engagements): ?>
-                        <table class='table table-bordered table-striped' id='engagements'>
+    <div class="container-fluid">
+        <div class="row">
+            <main class="ms-sm-auto px-md-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom print">
+                    <h1 class="h2">My Volunteers</h1>
+                    <div class="btn-toolbar mb-2 mb-md-0 no-print">
+                        <div class="btn-group me-2">
+                            <a href="affiliations.php" class="btn btn-sm btn-outline-secondary">Go back</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Search Bar -->
+                <input class="form-control my-3" id="searchInput" type="text" placeholder="Search">
+
+                <!-- Volunteers Table -->
+                <?php if ($engagements): ?>
+                    <div class="table-responsive">
+                        <table class='table' id='engagements'>
                             <thead>
                                 <tr>
-                                    <th>Event Name</th>
-                                    <th>Event Description</th>
-                                    <th>Opportunity Name</th>
+                                    <th>Event</th>
+                                    <th>Description</th>
+                                    <th>Opportunity</th>
                                     <th>Contact Info</th>
                                     <th>Event Duration</th>
                                     <th>Value</th>
@@ -83,32 +86,177 @@ $engagements = $obj->getEngagementsForAffiliatedVolunteer($_GET['volunteer_id'])
                                     <td>
                                         <?php echo $engagement['contribution_value']; ?>
                                     </td>
+                                    <?php
+                                    if(strcmp($engagement['status'],'1') == 0): 
+                                        echo "<td class='text-success'>Confirmed</td>";
+                                    elseif (strcmp($engagement['status'],'0') == 0): 
+                                        echo "<td class='text-danger'>Denied</td>";
+                                    else:
+                                        echo "<td class='text-muted'>Pending</td>";
+                                    endif;
+                                    ?>  
                                     <td>
-                                        <?php
-                                        if (strcmp($engagement['status'],'1') == 0):
-                                            echo "Confirmed";
-                                        elseif (strcmp($engagement['status'],'0') == 0):
-                                            echo "Denied";
-                                        else:
-                                            echo "Pending";
-                                        endif;
-                                        ?>
-                                    </td>   
-                                    <td>
-                                        <a href=<?php echo "engagement-delete.php?opportunity_id=".$engagement['opportunity_id']."&engagement_id=". $engagement['engagement_id']; ?> title='Delete Engagement' data-toggle='tooltip' style='color:red' class='btn btn-link' ><span class='glyphicon glyphicon-trash'></span> Delete</a>
+                                        <a href=<?php echo "engagement-delete.php?opportunity_id=".$engagement['opportunity_id']."&engagement_id=". $engagement['engagement_id']; ?> class='btn btn-danger btn-sm' ><span class='glyphicon glyphicon-trash'></span> Delete</a>
                                     </td>
                                 </tr>
                             <?php endforeach; ?>
                             </tbody>
                         </table>
+                        </div>
                     <?php else: ?>
                         <p class='lead'><em>No past engagements were found.</em></p>
                     <?php endif; ?>
-                </div>
-            </div>
+            </main>
+
+            <?php include "footer.php"; ?>
         </div>
     </div>
 
-    <?php include '../footer.php';?>
+    <script src="../assets/jQuery/jquery-3.4.1.min.js"></script>
+    <script src="../assets/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+
+    <script>
+    // Activate feather icon 
+    (function () {
+    'use strict'
+
+    feather.replace({ 'aria-hidden': 'true' })
+
+    })()
+
+    // search feature
+    $(document).ready(function(){
+    $("#searchInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#engagementTableBody tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+    });
+
+    // sort table alphabetically
+    function sortTable(n) {
+      var table, rows, switching, i, x, y, shouldSwitch, direction, switchcount = 0;
+      table = document.getElementById("affiliations");
+      switching = true;
+      // Set the sorting direction to ascending:
+      direction = "asc";
+      /* Make a loop that will continue until
+      no switching has been done: */
+      while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (i = 1; i < (rows.length - 1); i++) {
+          // Start by saying there should be no switching:
+          shouldSwitch = false;
+          /* Get the two elements you want to compare,
+          one from current row and one from the next: */
+          x = rows[i].getElementsByTagName("TD")[n];
+          y = rows[i + 1].getElementsByTagName("TD")[n];
+          /* Check if the two rows should switch place,
+          based on the direction, asc or desc: */
+          if (direction == "asc") {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+              // If so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          } else if (direction == "desc") {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+              // If so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          }
+        }
+        if (shouldSwitch) {
+          /* If a switch has been marked, make the switch
+          and mark that a switch has been done: */
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+          // Each time a switch is done, increase this count by 1:
+          switchcount ++;
+        } else {
+          /* If no switching has been done AND the direction is "asc",
+          set the direction to "desc" and run the while loop again. */
+          if (switchcount == 0 && direction == "asc") {
+            direction = "desc";
+            switching = true;
+          }
+        }
+      }
+    }
+
+    // sort table numerically
+    function sortTableNumerically(n) {
+      var table, rows, switching, i, x, y, shouldSwitch, direction, switchcount = 0;
+      table = document.getElementById("affiliations");
+      switching = true;
+      // Set the sorting direction to ascending:
+      direction = "asc";
+      /* Make a loop that will continue until
+      no switching has been done: */
+      while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (i = 1; i < (rows.length - 1); i++) {
+          // Start by saying there should be no switching:
+          shouldSwitch = false;
+          /* Get the two elements you want to compare,
+          one from current row and one from the next: */
+          x = rows[i].getElementsByTagName("TD")[n];
+          y = rows[i + 1].getElementsByTagName("TD")[n];
+          /* Check if the two rows should switch place,
+          based on the direction, asc or desc: */
+          if (direction == "asc") {
+            if (Number(x.innerHTML.toLowerCase()) > Number(y.innerHTML.toLowerCase())) {
+              // If so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          } else if (direction == "desc") {
+            if (Number(x.innerHTML.toLowerCase()) < Number(y.innerHTML.toLowerCase())) {
+              // If so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          }
+        }
+        if (shouldSwitch) {
+          /* If a switch has been marked, make the switch
+          and mark that a switch has been done: */
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+          // Each time a switch is done, increase this count by 1:
+          switchcount ++;
+        } else {
+          /* If no switching has been done AND the direction is "asc",
+          set the direction to "desc" and run the while loop again. */
+          if (switchcount == 0 && direction == "asc") {
+            direction = "desc";
+            switching = true;
+          }
+        }
+      }
+    }
+
+    // search feature
+    $(document).ready(function(){
+        $("#searchInput").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("#affiliationTableBody tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+    });
+    </script>
+
 </body>
 </html>
