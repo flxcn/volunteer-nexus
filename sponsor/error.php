d6c62954573e667d27c5d8302a7ffb19e2fc7806
Felix <30987125+flxcn@users.noreply.github.com>
2021-08-04 16:19:30 -0500

    Update error.php

--- sponsor/error.php
+++ sponsor/error.php
@@ -1,30 +1,47 @@
-<!DOCTYPE html>
+<?php
+session_start();
+
+if(!isset($_SESSION["volunteer_loggedin"]) || $_SESSION["volunteer_loggedin"] !== true){
+    header("location: sign-in.php");
+    exit;
+}
+?>
+
+<!doctype html>
 <html lang="en">
 <head>
-    <?php include '../head.php';?>
-    <meta charset="UTF-8">
-    <title>Somethings wrong...</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        .wrapper{
-            width: 750px;
-            margin: 0 auto;
-        }
-    </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Felix Chen">
+    
+    <title>Error</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../assets/css/main.css" rel="stylesheet">
 </head>
 <body>
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header">
-                        <h1>Something's wrong...</h1>
-                    </div>
-                    <div class="alert alert-danger fade in">
-                      <p>Look's like something went wrong. Click <a href="dashboard.php" class="alert-link">here</a> to go back to the dashboard. If the issue persists, please send an email to <a href="mailto:felix@volunteernexus.com?Subject=VolunteerNexus%20Issue%20Report" target="_blank">felix@volunteernexus.com</a> detailing what happened. </p>
+    <?php $thisPage=''; include 'navbar.php';?>
+
+    <div class="container-fluid">
+        <div class="row">
+            <main class="ms-sm-auto px-md-4">
+                <div class="row my-4">
+                    <div class="col-12 col-md-8 mx-auto mb-4 mb-lg-0">
+                        <div class="card border-danger">
+                            <h3 class="card-header bg-danger text-light">Something's wrong.</h3>
+                            <div class="card-body">
+                                <p>Look's like there's been an error. <a href="dashboard.php" class="btn-link">Click here</a> to go back to the dashboard. If the issue persists, please send an email to <a href="mailto:felix@volunteernexus.com?Subject=VolunteerNexus%20Issue%20Report" target="_blank">felix@volunteernexus.com</a> detailing what happened. </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </main>
+
+            <?php include "footer.php"; ?>
         </div>
     </div>
 </body>
