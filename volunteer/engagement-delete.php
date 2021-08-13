8828945df491d230b0b4ffcd90218eb4f22c0686
Felix <30987125+flxcn@users.noreply.github.com>
2021-08-13 17:05:27 -0500

    Update engagement-delete.php

--- volunteer/engagement-delete.php
+++ volunteer/engagement-delete.php
@@ -4,87 +4,82 @@ session_start();
 
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["volunteer_loggedin"]) || $_SESSION["volunteer_loggedin"] !== true){
-    header("location: login.php");
+    header("location: sign-in.php");
     exit;
 }
 
 // Process delete operation after confirmation
 if(isset($_POST["engagement_id"]) && !empty($_POST["engagement_id"])){
-  // Include config file
-  require_once "../config.php";
 
-  // Prepare a delete statement
-  $sql = "DELETE FROM engagements WHERE engagement_id = ? AND volunteer_id = ?";
+    require_once "../classes/VolunteerEngagementDeletion.php";
 
-  if($stmt = mysqli_prepare($link, $sql)){
-    // Bind variables to the prepared statement as parameters
-    mysqli_stmt_bind_param($stmt, "ii", $param_engagement_id, $param_volunteer_id);
-
-    // Set parameters
-    $param_engagement_id = trim($_POST["engagement_id"]);
-    $param_volunteer_id = trim($_SESSION["volunteer_id"]);
-
-    // Attempt to execute the prepared statement
-    if(mysqli_stmt_execute($stmt)){
-      // Records deleted successfully. Redirect to landing page
-      header("location: dashboard.php");
-      exit();
-    } else{
-      echo "Oops! Something went wrong. Please try again later.";
+    $volunteer_id = trim($_SESSION["volunteer_id"]);
+    $engagement_id = trim($_POST["engagement_id"]);
+    
+    $obj = new VolunteerEngagementDeletion($volunteer_id);
+    
+    if($obj->removeEngagement($engagement_id))
+    {
+        header("location: dashboard.php");
+        exit();
     }
-  }
-
-  // Close statement
-  mysqli_stmt_close($stmt);
-
-  // Close connection
-  mysqli_close($link);
-} else{
-  // Check existence of id parameter
-  if(empty(trim($_GET["engagement_id"]))){
-    // URL doesn't contain id parameter. Redirect to error page
-    header("location: error.php");
-    exit();
-  }
+    else {
+        header("location: error.php");
+        exit();
+    }
+}
+else {
+    // Check existence of id parameter
+    if(empty(trim($_GET["engagement_id"]))){
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+    }
 }
 ?>
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Felix Chen">
+    
     <title>Delete Engagement</title>
 
-        <!--Load required libraries-->
-        <?php include '../head.php'?>
-
-    <style type="text/css">
-        .wrapper{
-            width: 500px;
-            margin: 0 auto;
-        }
-    </style>
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Custom styles for this template -->
+    <link href="../assets/css/main.css" rel="stylesheet">
 </head>
 <body>
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header">
-                        <h1>Delete Engagement</h1>
-                    </div>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="alert alert-danger fade in">
+    <?php $thisPage='Events'; include 'navbar.php';?>
+
+    <div class="container-fluid">
+        <div class="row">
+            <main class="ms-sm-auto px-md-4">
+                <div class="col-12 col-md-6 card mt-3 mx-auto border-danger">
+                    <h5 class="card-header bg-danger text-light">Delete engagement</h5>
+                    <div class="card-body">
+                        <p>Are you sure you want to delete this engagement? This action cannot be undone.</p><br>
+                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                             <input type="hidden" name="engagement_id" value="<?php echo trim($_GET["engagement_id"]); ?>"/>
-                            <p>Are you sure you want to delete this engagement? This action cannot be undone.</p><br>
-                            <p>
-                                <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="dashboard.php" class="btn btn-default">No</a>
-                            </p>
-                        </div>
-                    </form>
+                            <input type="submit" value="Yes" class="btn btn-danger"> 
+                            <a href="dashboard.php" class="btn btn-outline-secondary">No</a>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            </main>
+
+            <?php include "footer.php"; ?>
         </div>
     </div>
+
+    <script src="../assets/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
+
+
