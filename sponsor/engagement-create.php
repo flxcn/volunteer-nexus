a75f5ff1a16defa94d11be61eeb82c60bfc9c900
Felix <30987125+flxcn@users.noreply.github.com>
2021-08-13 17:04:33 -0500

    Update engagement-create.php

--- sponsor/engagement-create.php
+++ sponsor/engagement-create.php
@@ -8,7 +8,6 @@ if(!isset($_SESSION["sponsor_loggedin"]) || $_SESSION["sponsor_loggedin"] !== tr
     exit;
 }
 
-
 // Define and intialize variables
 $sponsor_id = $_SESSION["sponsor_id"];
 $volunteer_id = "";
@@ -43,43 +42,40 @@ $jsonOpportunities = $engagementFormPopulatorObj->getOpportunities();
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
 
-  // Instatiate EngagementCreation object
-  require_once '../classes/EngagementCreation.php';
-  $engagementCreationObj = new EngagementCreation($sponsor_id);
+    // Instatiate EngagementCreation object
+    require_once '../classes/EngagementCreation.php';
+    $engagementCreationObj = new EngagementCreation($sponsor_id);
 
-  // Validate volunteer_id from "volunteer_name" selector 
-  $volunteer_id = trim($_POST["volunteer_name"]);
-  $volunteer_name_error = $engagementCreationObj->setVolunteerId($volunteer_id);
+    // Validate volunteer_id from "volunteer_name" selector 
+    $volunteer_id = trim($_POST["volunteer_name"]);
+    $volunteer_name_error = $engagementCreationObj->setVolunteerId($volunteer_id);
 
-  // Validate event_id from "event_id" selector
-  $event_id = trim($_POST["event_name"]);
-  $event_name_error = $engagementCreationObj->setEventId($event_id);
- 
-  // Validate opportunity_id and contribution value from "opportunity_name" selector
-  $opportunity_values = json_decode($_POST["opportunity_name"]);
-  $opportunity_id = $opportunity_values[0];
-  $opportunity_name_error = $engagementCreationObj->setOpportunityId($opportunity_id);
-  $contribution_value = $_POST["contribution_value"];
-  $opportunity_name_error = $engagementCreationObj->setContributionValue($contribution_value);
-  
-  // Set status of whether the engagement needs verification
-  $status = trim($_POST["status"]);
-  $engagementCreationObj->setStatus($status);
+    // Validate event_id from "event_id" selector
+    $event_id = trim($_POST["event_name"]);
+    $event_name_error = $engagementCreationObj->setEventId($event_id);
+    
+    // Validate opportunity_id and contribution value from "opportunity_name" selector
+    $opportunity_values = json_decode($_POST["opportunity_name"]);
+    $opportunity_id = $opportunity_values[0];
+    $opportunity_name_error = $engagementCreationObj->setOpportunityId($opportunity_id);
+    $contribution_value = $_POST["contribution_value"];
+    $opportunity_name_error = $engagementCreationObj->setContributionValue($contribution_value);
+    
+    // Set status of whether the engagement needs verification
+    $status = trim($_POST["status"]);
+    $engagementCreationObj->setStatus($status);
 
-  // Set sponsor_id
-  // $sponsor_id = $_SESSION["sponsor_id"];
-
-  // Check input errors before inserting in database
-  if(empty($sponsor_id_error) && empty($volunteer_name_error) && empty($event_name_error) && empty($opportunity_name_error) && empty($contribution_value_error) && empty($status_error)) 
-  {
-    if($engagementCreationObj->addEngagement()) {
-      header("Location: dashboard.php");
-      exit();
+    // Check input errors before inserting in database
+    if(empty($sponsor_id_error) && empty($volunteer_name_error) && empty($event_name_error) && empty($opportunity_name_error) && empty($contribution_value_error) && empty($status_error)) 
+    {
+        if($engagementCreationObj->addEngagement()) {
+            header("Location: dashboard.php");
+            exit();
+        }
+        else {
+            echo "Something went wrong. Please try again later. If the issue persists, send an email to felix@volunteernexus.com detailing the problem.";
+        }
     }
-    else {
-      echo "Something went wrong. Please try again later. If the issue persists, send an email to felix@volunteernexus.com detailing the problem.";
-    }
-  }
 }
 ?>
 
