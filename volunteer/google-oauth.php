640802f3ec62405ba9858a5c4ad7268c02d42949
Felix <30987125+flxcn@users.noreply.github.com>
2021-08-24 12:30:52 -0500

    Update google-oauth.php

--- volunteer/google-oauth.php
+++ volunteer/google-oauth.php
@@ -46,7 +46,6 @@ if (isset($_GET['code'])) {
         $_SESSOPM["username"] = $obj->getUsername();
         $_SESSION["first_name"] = $obj->getFirstName();
         $_SESSION["last_name"] = $obj->getLastName();
-        //$_SESSION["graduation_year"] = $obj->getGraduationYear();
 
         // Redirect user to dashboard
         header("location: dashboard.php");
@@ -56,15 +55,5 @@ if (isset($_GET['code'])) {
 
     $auth_url = $client->createAuthUrl();
 
-    // echo "<br>
-    //     <div class='col s12 m6 offset-m3 center-align'>
-    //     <a class='oauth-container btn darken-4 white black-text' href='".$auth_url ."' style='text-transform:none; background-color: white; border-color: black;'>
-    //         <div class='left'>
-    //         <img width='20px' style='margin-top:7px; margin-right:8px' alt='Google sign-in' src='../assets/images/google-g-logo.png' />
-    //         </div>
-    //         Login with Google
-    //     </a>
-    //     </div>
-    //     <br>";
 }
 ?>
