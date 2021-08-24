19aad4332541aa9881b8486b1624d7be6ac15285
Felix <30987125+flxcn@users.noreply.github.com>
2021-08-24 12:30:00 -0500

    Update engagement-create.php

--- volunteer/engagement-create.php
+++ volunteer/engagement-create.php
@@ -4,7 +4,7 @@ session_start();
 
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["volunteer_loggedin"]) || $_SESSION["volunteer_loggedin"] !== true){
-    header("location: login.php");
+    header("location: sign-in.php");
     exit;
 }
 
@@ -35,31 +35,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         else {
             if($obj->addEngagement()) {
                 // Success
-
                 $_SESSION["engagement_creation_success"] = true;
-
                 header("location: dashboard.php");
-
-                // $url = 'dashboard.php';
-                // $data = array('creation' => 'success');
-
-                // // use key 'http' even if you send the request to https://...
-                // $options = array(
-                //     'http' => array(
-                //         'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                //         'method'  => 'POST',
-                //         'content' => http_build_query($data)
-                //     )
-                // );
-                // $context  = stream_context_create($options);
-                // $result = file_get_contents($url, false, $context);
-                // if ($result === FALSE) { /* Handle error */ }
-
-                // var_dump($result);
-
                 exit();
-
-
             }
             else {
                 // Failure
