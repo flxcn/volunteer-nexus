28bcdbb34e10d0983718a6ca9e2ba97076ad931a
Felix <30987125+flxcn@users.noreply.github.com>
2021-09-19 20:12:00 -0400

    Update DatabaseConnection.php

--- classes/DatabaseConnection.php
+++ classes/DatabaseConnection.php
@@ -1,15 +1,18 @@
 <?php
+// Error Report Settings
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 ini_set('log_errors',1);
 error_reporting(E_ALL);
 
+// Database Credentials
 define('DB_HOST','localhost');
 define('DB_NAME','volunteer_nexus');
 define('DB_CHARSET','utf8mb4');
 define('DB_USERNAME','root');
 define('DB_PASSWORD','root');
 
+// Timezone
 date_default_timezone_set('America/Chicago');
 
 class DatabaseConnection
@@ -17,14 +20,15 @@ class DatabaseConnection
     protected static $instance;
     protected $pdo;
 
-    public function __construct() {
-			$options = [
-					PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-					PDO::ATTR_EMULATE_PREPARES   => false,
-			];
-      $dsn = 'mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset='.DB_CHARSET;
-      $this->pdo = new PDO($dsn, DB_USERNAME, DB_PASSWORD, $options);
+    public function __construct() 
+    {
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $dsn = 'mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset='.DB_CHARSET;
+        $this->pdo = new PDO($dsn, DB_USERNAME, DB_PASSWORD, $options);
     }
 
     public static function instance()
